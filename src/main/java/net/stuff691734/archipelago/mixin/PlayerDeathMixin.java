abe472931d700e35935fd7a685e949c9787b9af5
Stuff691734 <23fuelln@gmail.com>
2026-01-13 14:26:16 -0600

    change OnDeathEvent to a Mixin

--- src/main/java/net/stuff691734/archipelago/mixin/PlayerDeathMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/PlayerDeathMixin.java
@@ -2,7 +2,7 @@ package net.stuff691734.archipelago.mixin;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.stuff691734.archipelago.events.OnDeathEvent;
+import net.stuff691734.archipelago.Archipelago;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,8 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public class PlayerDeathMixin {
+
     @Inject(at = @At(value = "TAIL"), method = "onDeath")
     private void onDeath(DamageSource source, CallbackInfo ci) {
-        OnDeathEvent.EVENT.invoker().interact((ServerPlayerEntity)(Object)this, source);
+        if (source == DamageSource.OUT_OF_WORLD) {
+            // no looping hopefully
+            Archipelago.client.sendDeathlink(
+                    Archipelago.client.getMyName(),
+                    source.getDeathMessage((ServerPlayerEntity)(Object)this).getString()
+            );
+        }
     }
+
 }
