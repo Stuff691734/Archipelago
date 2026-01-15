49db3cbb8968e5699954f004aa8df6e1dae0c4d1
Stuff691734 <23fuelln@gmail.com>
2026-01-14 22:37:29 -0600

    fully implement DeathLink improved item giving logic

--- src/main/java/net/stuff691734/archipelago/ArchipelagoListeners.java
+++ src/main/java/net/stuff691734/archipelago/ArchipelagoListeners.java
@@ -2,9 +2,6 @@ package net.stuff691734.archipelago;
 
 import com.google.gson.JsonObject;
 import io.github.archipelagomw.events.*;
-import net.minecraft.advancement.Advancement;
-import net.minecraft.advancement.AdvancementEntry;
-import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
@@ -16,11 +13,18 @@ public class ArchipelagoListeners {
         ChecksState checksState = ChecksState.getServerState(Archipelago.server);
 
         slotData.asMap().forEach((key, value) -> checksState.slotData.put(key, value.getAsString()));
+        if (checksState.slotData.get("death_link").equals("1")) {
+            Archipelago.LOGGER.info("DeathLink activated");
+            Archipelago.client.setDeathLinkEnabled(true);
+            Archipelago.client.addTag("DeathLink");
+
+        }
         Archipelago.LOGGER.info(checksState.slotData.toString());
     }
 
     @ArchipelagoEventListener
     public void onDeathLink(DeathLinkEvent event) {
+        Utils.sendMessage(Text.literal(String.format("[DeathLink] %s died: %s",event.source, event.cause)));
         for (ServerPlayerEntity player : Archipelago.server.getPlayerManager().getPlayerList()) {
             player.kill(player.getEntityWorld());
         }
@@ -46,7 +50,12 @@ public class ArchipelagoListeners {
             }
             else {
                 for (ServerPlayerEntity player : Archipelago.server.getPlayerManager().getPlayerList()) {
-                    Utils.giveItem(player, event.getItemName());
+                    int playerLastCheck = ChecksState.getServerState(Archipelago.server).playerLastCheck.getOrDefault(player.getUuidAsString(), 0);
+                    if (event.getIndex() > playerLastCheck) {
+
+                        ChecksState.getServerState(Archipelago.server).playerLastCheck.put(player.getUuidAsString(), (int)event.getIndex());
+                        Utils.giveItem(player, event.getItemName());
+                    }
                 }
             }
         }
