a0ff5e924455d5da1f3d9e6c8f7f0b8ed1b6f064
Stuff691734 <23fuelln@gmail.com>
2026-01-24 16:22:55 -0600

    use java 8 as it works for all versions

--- src/main/java/net/stuff691734/archipelago/archipelagoData/Check.java
+++ src/main/java/net/stuff691734/archipelago/archipelagoData/Check.java
@@ -1,4 +1,10 @@
 package net.stuff691734.archipelago.archipelagoData;
 
-public record Check(String type, String parent_id) {
+public class Check {
+    public String type;
+    public String parent_id;
+    public Check(String type, String parent_id) {
+        this.type = type;
+        this.parent_id = parent_id;
+    }
 }
