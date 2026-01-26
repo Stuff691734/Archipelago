9e0f194bab0ce0c6d15b1a2f3eae6ba4054c1af0
Stuff691734 <23fuelln@gmail.com>
2026-01-26 16:50:18 -0600

    change Logger for java 8 support

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -8,12 +8,12 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
 
 public class Archipelago implements ModInitializer {
     public static final String MOD_ID = "archipelago";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = java.util.logging.Logger.getLogger(MOD_ID);
     public static ArchipelagoClient client;
     public static MinecraftServer server;
 
