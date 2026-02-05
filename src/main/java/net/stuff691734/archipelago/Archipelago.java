19b40bba6e8b2b23803f8d2a7b9b98b2de228442
Stuff691734 <23fuelln@gmail.com>
2026-02-05 16:46:29 -0600

    improve/rework tree based logic to match archipelago logic

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -43,7 +43,7 @@ public class Archipelago implements ModInitializer {
                     ChecksState.getServerState(server).playerLastCheck.put(player.getUuidAsString(), serverLastCheck);
 
                     for (NetworkItem item: client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
-                        if (Utils.isRootAdvancementId(item.itemName)) {
+                        if (Utils.isAdvancementId(item.itemName)) {
                             ChecksState.getServerState(Archipelago.server).checks.put(item.itemName, true);
                         }
                         else {
