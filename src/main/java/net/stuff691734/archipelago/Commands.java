736b246af567db74d89b8ec869c9a2034c8c3f92
Stuff691734 <23fuelln@gmail.com>
2026-02-05 22:08:36 -0600

    adjust recipe check

--- src/main/java/net/stuff691734/archipelago/Commands.java
+++ src/main/java/net/stuff691734/archipelago/Commands.java
@@ -66,14 +66,12 @@ public class Commands {
                                 if (parent != null) {
                                     parent_id = parent.holder().id().toString();
                                 }
-                                if (parent_id != null && parent_id.equals("minecraft:recipes/root")) {
-                                    continue;
+                                if (parent_id == null || !parent_id.equals("minecraft:recipes/root")) {
+                                    checks.put(advancement.id().toString(), new Check(
+                                            display.getType().getSerializedName(),
+                                            parent_id
+                                    ));
                                 }
-
-                                checks.put(advancement.id().toString(),new Check(
-                                        display.getType().getSerializedName(),
-                                        parent_id
-                                ));
                             }
                         });
                     }
