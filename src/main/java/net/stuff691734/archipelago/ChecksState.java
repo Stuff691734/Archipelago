f3b0296c46bc786a3944889b7870922f91cb510e
Stuff691734 <23fuelln@gmail.com>
2026-02-10 12:56:02 -0600

    changes to work with 1.21.5, changes to persistent state

--- src/main/java/net/stuff691734/archipelago/ChecksState.java
+++ src/main/java/net/stuff691734/archipelago/ChecksState.java
@@ -1,9 +1,10 @@
 package net.stuff691734.archipelago;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.saveddata.SavedDataType;
 import net.minecraft.world.level.storage.DimensionDataStorage;
 
 import java.util.HashMap;
@@ -15,43 +16,37 @@ public class ChecksState extends SavedData {
     public Map<String, String> slotData = new HashMap<>();
     public Map<String, Integer> playerLastCheck = new HashMap<>();
 
+    private static final Codec<ChecksState> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+            Codec.unboundedMap(Codec.STRING, Codec.BOOL).fieldOf("checks").forGetter(ChecksState::getChecks),
+            Codec.unboundedMap(Codec.STRING, Codec.STRING).fieldOf("slot_data").forGetter(ChecksState::getSlotData),
+            Codec.unboundedMap(Codec.STRING, Codec.INT).fieldOf("player_last_check").forGetter(ChecksState::getPlayerLastCheck)
+        ).apply(instance, ChecksState::create)
+    );
 
-    @Override
-    public CompoundTag save(CompoundTag nbt, HolderLookup.Provider registryLookup) {
-        CompoundTag archipelagoNbt = new CompoundTag();
-
-        CompoundTag checksNbt = new CompoundTag();
-        checks.forEach(checksNbt::putBoolean);
-
-        CompoundTag slotDataNbt = new CompoundTag();
-        slotData.forEach(slotDataNbt::putString);
-
-        CompoundTag playerLastCheckDataNbt = new CompoundTag();
-        playerLastCheck.forEach(playerLastCheckDataNbt::putInt);
-
-        archipelagoNbt.put("checks", checksNbt);
-        archipelagoNbt.put("slot_data", slotDataNbt);
-        archipelagoNbt.put("player_last_check", playerLastCheckDataNbt);
-
-        nbt.put("archipelago", archipelagoNbt);
-
-        return nbt;
-    }
-
-    public static ChecksState createFromNbt(CompoundTag tag, HolderLookup.Provider registryLookup) {
+    public static ChecksState create(
+            Map<String, Boolean> checksMap,
+                              Map<String, String> slotDataMap,
+                              Map<String, Integer> playerLastCheckMap
+        ) {
         ChecksState state = new ChecksState();
-        CompoundTag archipelagoNbt = tag.getCompound("archipelago");
-
-        CompoundTag checksNbt = archipelagoNbt.getCompound("checks");
-        checksNbt.getAllKeys().forEach(key -> state.checks.put(key, checksNbt.getBoolean(key)));
-
-        CompoundTag slotDataNbt = archipelagoNbt.getCompound("slot_data");
-        slotDataNbt.getAllKeys().forEach(key -> state.slotData.put(key, slotDataNbt.getString(key)));
-
-        CompoundTag playerLastCheckDataNbt = archipelagoNbt.getCompound("player_last_check");
-        playerLastCheckDataNbt.getAllKeys().forEach(key -> state.playerLastCheck.put(key, playerLastCheckDataNbt.getInt(key)));
+        // gives ImmutableMap
+        state.checks = new HashMap<>(checksMap);
+        state.slotData = new HashMap<>(slotDataMap);
+        state.playerLastCheck = new HashMap<>(playerLastCheckMap);
 
         return state;
+    }
+
+    public Map<String, Boolean> getChecks() {
+        return checks;
+    }
+
+    public Map<String, String> getSlotData() {
+        return slotData;
+    }
+
+    public Map<String, Integer> getPlayerLastCheck() {
+        return playerLastCheck;
     }
 
     public static ChecksState createNew() {
@@ -62,16 +57,17 @@ public class ChecksState extends SavedData {
         return state;
     }
 
-    private static final Factory<ChecksState> type = new Factory<>(
+    private static final SavedDataType<ChecksState> type = new SavedDataType<>(
+            Archipelago.MODID,
             ChecksState::createNew,
-            ChecksState::createFromNbt,
+            CODEC,
             null
     );
 
     public static ChecksState getServerState(MinecraftServer server) {
         DimensionDataStorage persistentStateManager = server.overworld().getDataStorage();
 
-        ChecksState state = persistentStateManager.computeIfAbsent(type, Archipelago.MODID);
+        ChecksState state = persistentStateManager.computeIfAbsent(type);
 
         state.setDirty();
 
