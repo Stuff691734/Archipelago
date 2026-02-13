3f351a4154c95205f46fdfa583d7152ab7c024e2
Stuff691734 <23fuelln@gmail.com>
2026-02-13 01:33:02 -0600

    switch from slf4j.Logger to apache.logging.log4j.LogManager

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -11,8 +11,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Archipelago.MODID)
@@ -20,7 +21,7 @@ public class Archipelago {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "archipelago";
     // Directly reference a slf4j logger
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
     public static ArchipelagoClient client;
     public static MinecraftServer server;
 
