d824d99105d60b54b65aaf3290a1cc67d4325e23
Stuff691734 <23fuelln@gmail.com>
2026-03-24 12:14:18 -0500

    updates from version 2.0 to 2.2.2, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/net/ClaimAllRewardsMessageMixin.java
@@ -0,0 +1,17 @@
+package net.stuff691734.archipelago.mixin.FTBQuests.net;
+
+import dev.ftb.mods.ftbquests.net.ClaimAllRewardsMessage;
+import dev.ftb.mods.ftbquests.quest.QuestObject;
+import dev.ftb.mods.ftbquests.quest.TeamData;
+import net.stuff691734.archipelago.ftbquests.FTBUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClaimAllRewardsMessage.class)
+public class ClaimAllRewardsMessageMixin {
+    @Redirect(method = "lambda$handle$2", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbquests/quest/TeamData;isCompleted(Ldev/ftb/mods/ftbquests/quest/QuestObject;)Z"))
+    private static boolean isCompleted(TeamData teamData, QuestObject object) {
+        return FTBUtils.hasQuestRewardAccess(teamData, object);
+    }
+}
