a8052c84a823719026d7be89fa4fff0ac311d872
Stuff691734 <23fuelln@gmail.com>
2026-03-25 12:00:39 -0500

    changes to work with 1.19.4

--- src/main/java/net/stuff691734/archipelago/commands/GetCommand.java
+++ src/main/java/net/stuff691734/archipelago/commands/GetCommand.java
@@ -8,19 +8,19 @@ import net.stuff691734.archipelago.Archipelago;
 
 public class GetCommand {
     public static int execute(CommandContext<CommandSourceStack> context) {
-        context.getSource().sendSuccess(() -> Component.literal(Archipelago.archipelagoPersistentState.advancementChecks.toString()), true);
-        context.getSource().sendSuccess(() -> Component.literal(Archipelago.archipelagoPersistentState.ftbQuestChecks.toString()), true);
-        context.getSource().sendSuccess(() -> Component.literal(Archipelago.archipelagoPersistentState.slotData.toString()), true);
-        context.getSource().sendSuccess(() -> Component.literal(Archipelago.client.getItemManager().getReceivedItemIDs().toString()), true);
+        context.getSource().sendSuccess(Component.literal(Archipelago.archipelagoPersistentState.advancementChecks.toString()), true);
+        context.getSource().sendSuccess(Component.literal(Archipelago.archipelagoPersistentState.ftbQuestChecks.toString()), true);
+        context.getSource().sendSuccess(Component.literal(Archipelago.archipelagoPersistentState.slotData.toString()), true);
+        context.getSource().sendSuccess(Component.literal(Archipelago.client.getItemManager().getReceivedItemIDs().toString()), true);
         return 0;
     }
 
     public static int executeSpecific(CommandContext<CommandSourceStack> context) {
         final String checkName = StringArgumentType.getString(context, "check");
         if (checkName.startsWith("adv ")) {
-            context.getSource().sendSuccess(() -> Component.literal(Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkName.substring(4), false).toString()), true);
+            context.getSource().sendSuccess(Component.literal(Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkName.substring(4), false).toString()), true);
         } else {
-            context.getSource().sendSuccess(() -> Component.literal(Archipelago.archipelagoPersistentState.ftbQuestChecks.getOrDefault(checkName.substring(4), false).toString()), true);
+            context.getSource().sendSuccess(Component.literal(Archipelago.archipelagoPersistentState.ftbQuestChecks.getOrDefault(checkName.substring(4), false).toString()), true);
         }
         return 0;
     }
