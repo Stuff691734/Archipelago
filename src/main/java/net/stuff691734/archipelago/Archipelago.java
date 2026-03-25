29dcb5aa9deef85052c7b27ea4a1a58ea1ec7915
Stuff691734 <23fuelln@gmail.com>
2026-03-25 14:22:08 -0500

    changes to work with 1.19.3, DamageSource instead of DamageType

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -1,10 +1,7 @@
 package net.stuff691734.archipelago;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.common.MinecraftForge;
 import net.stuff691734.archipelago.events.neoforge.ForgeEvents;
@@ -20,10 +17,8 @@ public class Archipelago {
     public static ArchipelagoPersistentState archipelagoPersistentState;
     public static SlotData slotData = new SlotData();
 
-    public static final ResourceKey<DamageType> DeathLinkDamage = ResourceKey.create(
-        Registries.DAMAGE_TYPE,
-        ResourceLocation.fromNamespaceAndPath(MODID, "death_link")
-    );
+    public static final DamageSource DeathLinkDamage = new DamageSource(MODID + ".death_link").bypassInvul();
+
 
     public Archipelago() {
         ForgeEvents.register(MinecraftForge.EVENT_BUS);
