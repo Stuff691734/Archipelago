bd94533df1ad34bc48224d3f989f23bc3d5aec80
Stuff691734 <23fuelln@gmail.com>
2026-03-25 11:48:30 -0500

    updates from version 2.0 to 2.2.2 without ftb quests, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/commands/AddCommand.java
@@ -0,0 +1,16 @@
+package net.stuff691734.archipelago.commands;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.events.archipealgo.ReceiveItemEvent;
+
+public class AddCommand {
+    public static int execute(CommandContext<CommandSourceStack> context) {
+        final String checkName = StringArgumentType.getString(context, "check");
+        String[] itemName = checkName.split(" ",2);
+        ReceiveItemEvent.parseItem(itemName[0], itemName[1], null);
+        return 0;
+    }
+}
---
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/ReceiveItemEvent.java
@@ -0,0 +1,81 @@
+package net.stuff691734.archipelago.events.archipealgo;
+
+import io.github.archipelagomw.events.ArchipelagoEventListener;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.Utils;
+
+import javax.annotation.Nullable;
+
+public class ReceiveItemEvent {
+    @ArchipelagoEventListener
+    public void onReceiveItems(io.github.archipelagomw.events.ReceiveItemEvent event) {
+        if (Archipelago.server != null) {
+            Utils.sendMessage(Component.literal(String.format(
+                    "Received %s from %s (%s)",
+                    event.getItemName(),
+                    event.getPlayerName(),
+                    event.getLocationName()
+            )));
+            String[] itemName = event.getItemName().split(" ",2);
+
+            ReceiveItemEvent.parseItem(itemName[0], itemName[1], event.getIndex());
+        }
+    }
+
+    public static void parseItem(String itemType, String itemName, @Nullable Long index) {
+        for (ServerPlayer player : Archipelago.server.getPlayerList().getPlayers()) {
+            playerParseItem(player, itemType, itemName, index);
+        }
+        switch (itemType) {
+            case "adv":
+                if (Utils.isAdvancementId(itemName)) {
+                    Archipelago.archipelagoPersistentState.advancementChecks.put(itemName, true);
+                }
+                break;
+            case "ftb":
+                Archipelago.LOGGER.error("Got FTB check on version without FTB Quests");
+                break;
+        }
+        Archipelago.archipelagoPersistentState.setDirty();
+    }
+
+    public static void playerParseItem(ServerPlayer player, String itemType, String itemName, @Nullable Long index) {
+        if (
+            index != null &&
+            Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(player.getStringUUID(),0) >= index
+        ) {
+            return;
+        }
+        if (index != null) {
+            Archipelago.archipelagoPersistentState.playerLastCheck.put(player.getStringUUID(), index.intValue());
+        }
+
+        switch (itemType) {
+            case "adv":
+                if (Utils.isAdvancementId(itemName)) {
+                    Archipelago.archipelagoPersistentState.advancementChecks.put(itemName, true);
+                    Advancement advancement = Archipelago.server.getAdvancements().getAdvancement(new ResourceLocation(itemName));
+                    if (Archipelago.slotData.isInitiated && Archipelago.slotData.advancement_checks_give_items) {
+                        assert advancement != null; // via isAdvancementId
+                        DisplayInfo display = advancement.getDisplay();
+                        if (display != null) {
+                            Utils.giveItem(player, display.getIcon().toString());
+                        }
+                    }
+                }
+                break;
+            case "ftb":
+                break;
+            case "item":
+                if (Utils.isItemId(itemName)) {
+                    Utils.giveItem(player, itemName);
+                }
+                break;
+        }
+    }
+}
