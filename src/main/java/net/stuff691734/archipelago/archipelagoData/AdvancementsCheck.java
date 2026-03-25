1a3015f2a333b332c9df8607d44586a4d33fd5e9
Stuff691734 <23fuelln@gmail.com>
2026-03-25 11:48:30 -0500

    updates from version 2.0 to 2.2.2 without ftb quests, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/archipelagoData/AdvancementsCheck.java
@@ -0,0 +1,7 @@
+package net.stuff691734.archipelago.archipelagoData;
+
+public class AdvancementsCheck extends Check{
+    public AdvancementsCheck(String type, String parent_id) {
+        super(type, new String[]{parent_id});
+    }
+}
