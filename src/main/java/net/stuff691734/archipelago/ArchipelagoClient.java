b863f4eb6457f45b7460bb76ff9fa509bb42e954
Stuff691734 <23fuelln@gmail.com>
2026-03-28 01:16:46 -0500

    fix Component.literal no longer existing

--- src/main/java/net/stuff691734/archipelago/ArchipelagoClient.java
+++ src/main/java/net/stuff691734/archipelago/ArchipelagoClient.java
@@ -2,12 +2,13 @@ package net.stuff691734.archipelago;
 
 import io.github.archipelagomw.Client;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 
 public class ArchipelagoClient extends Client {
     @Override
     public void onError(Exception ex) {
         Archipelago.LOGGER.info(ex.getLocalizedMessage());
-        Utils.sendMessage(Component.literal(ex.getLocalizedMessage()));
+        Utils.sendMessage(new TextComponent(ex.getLocalizedMessage()));
     }
 
     @Override
--- src/main/java/net/stuff691734/archipelago/events/archipealgo/ArchipelagoMessageEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/ArchipelagoMessageEvent.java
@@ -3,11 +3,12 @@ package net.stuff691734.archipelago.events.archipealgo;
 import io.github.archipelagomw.events.ArchipelagoEventListener;
 import io.github.archipelagomw.events.PrintJSONEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.stuff691734.archipelago.Utils;
 
 public class ArchipelagoMessageEvent {
     @ArchipelagoEventListener
     public void onEvent(PrintJSONEvent event) {
-        Utils.sendMessage(Component.literal(event.apPrint.getPlainText()));
+        Utils.sendMessage(new TextComponent(event.apPrint.getPlainText()));
     }
 }
