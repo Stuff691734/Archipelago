572085c2f3f64b341cc1b9642dc8ba50805cd413
Stuff691734 <23fuelln@gmail.com>
2026-03-28 00:01:14 -0500

    add logs to chat when connecting fails

--- src/main/java/net/stuff691734/archipelago/ArchipelagoClient.java
+++ src/main/java/net/stuff691734/archipelago/ArchipelagoClient.java
@@ -1,11 +1,13 @@
 package net.stuff691734.archipelago;
 
 import io.github.archipelagomw.Client;
+import net.minecraft.network.chat.Component;
 
 public class ArchipelagoClient extends Client {
     @Override
     public void onError(Exception ex) {
         Archipelago.LOGGER.info(ex.getLocalizedMessage());
+        Utils.sendMessage(Component.literal(ex.getLocalizedMessage()));
     }
 
     @Override
