13a877fc26189a2559ef6c2da60cf9eb39e3605a
Stuff691734 <23fuelln@gmail.com>
2026-03-28 00:28:28 -0500

    improve checks to disable logic when quests should not be randomized

--- src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/client/gui/quests/RewardButtonMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/client/gui/quests/RewardButtonMixin.java
@@ -3,7 +3,6 @@ package net.stuff691734.archipelago.mixin.FTBQuests.client.gui.quests;
 import dev.ftb.mods.ftbquests.client.gui.quests.RewardButton;
 import dev.ftb.mods.ftbquests.quest.QuestObject;
 import dev.ftb.mods.ftbquests.quest.TeamData;
-import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.ftbquests.FTBUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class RewardButtonMixin {
     @Redirect(method = "draw", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbquests/quest/TeamData;isCompleted(Ldev/ftb/mods/ftbquests/quest/QuestObject;)Z"))
     private static boolean modifyRewardAccess(TeamData teamData, QuestObject questObject) {
-        return FTBUtils.hasQuestRewardAccess(teamData, questObject);
+        return FTBUtils.hasQuestRewardAccess(questObject, teamData::isCompleted);
     }
 
     @Redirect(method = "getWidgetType", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbquests/quest/TeamData;isCompleted(Ldev/ftb/mods/ftbquests/quest/QuestObject;)Z"))
-    public boolean getWidgetType(TeamData teamData, QuestObject object) {
-        return FTBUtils.hasQuestRewardAccess(teamData, object);
+    public boolean getWidgetType(TeamData teamData, QuestObject questObject) {
+        return FTBUtils.hasQuestRewardAccess(teamData, questObject);
     }
 }
