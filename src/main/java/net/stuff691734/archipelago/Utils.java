b965ab86ac91069f879ad4d8ffb7ac01fa203136
Stuff691734 <23fuelln@gmail.com>
2026-03-29 23:38:13 -0500

    only show advancements that the player can complete and show all advancements the player can complete

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -3,6 +3,7 @@ package net.stuff691734.archipelago;
 import com.mojang.serialization.DataResult;
 import net.minecraft.advancements.AdvancementNode;
 import net.minecraft.advancements.AdvancementTree;
+import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -10,6 +11,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Objects;
 
 public class Utils {
     public static boolean isAdvancementId(String advancementId) {
@@ -52,6 +54,59 @@ public class Utils {
 
         for(ServerPlayer player : Archipelago.server.getPlayerList().getPlayers()) {
             player.sendSystemMessage(message);
+        }
+    }
+
+    public static boolean shouldAdvancementBeHidden(DisplayInfo display, AdvancementNode advancement) {
+        if (
+            Archipelago.slotData.isInitiated &&
+            (
+                !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
+            )
+        ) {
+            if (
+                Archipelago.slotData.activated_modules.contains("Advancements") &&
+                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
+            ) {
+                return true;
+            }
+
+            return display.isHidden();
+        }
+
+        if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
+            AdvancementNode rootAdvancement = AdvancementNode.getRoot(advancement);
+            String rootAdvancementName = rootAdvancement.holder().id().toString();
+
+            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(rootAdvancementName, false);
+        }
+        // parent advancement
+        else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
+            if (AdvancementNode.getRoot(advancement) == advancement) {
+                // if root check against self
+                return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
+            } else {
+                // otherwise check against values up tree not including self
+                AdvancementNode checkAdvancement = advancement;
+                // exits when all advancements up the tree have been checked
+                while (checkAdvancement != null) {
+                    checkAdvancement = checkAdvancement.parent();
+
+                    if (checkAdvancement != null) {
+                        String checkAdvancementName = checkAdvancement.holder().id().toString();
+                        if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkAdvancementName, false)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        // not either tab or tree... invalid/notstarted, going to check against self as I eventually want
+        // to do an advancement insanity thing
+        else {
+            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
         }
     }
 }
---
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementsScreenMixin.java
@@ -0,0 +1,22 @@
+package net.stuff691734.archipelago.mixin;
+
+import net.minecraft.advancements.AdvancementNode;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.advancements.AdvancementTab;
+import net.minecraft.client.gui.screens.advancements.AdvancementsScreen;
+import net.stuff691734.archipelago.Archipelago;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(AdvancementsScreen.class)
+public class AdvancementsScreenMixin {
+    @Redirect(method = "onAddAdvancementRoot", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/advancements/AdvancementTab;create(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/screens/advancements/AdvancementsScreen;ILnet/minecraft/advancements/AdvancementNode;)Lnet/minecraft/client/gui/screens/advancements/AdvancementTab;"))
+    public AdvancementTab avoidAddingEmptyPages(Minecraft minecraft, AdvancementsScreen screen, int index, AdvancementNode advancementNode) {
+        AdvancementTab advancementTab = AdvancementTab.create(minecraft, screen, index, advancementNode);
+        if (advancementTab == null || (Archipelago.slotData.isInitiated && !Archipelago.slotData.activated_modules.contains("Advancements"))) {
+            return advancementTab;
+        }
+        return Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancementTab.getRootNode().holder().id().toString(), false) ? advancementTab : null;
+    }
+}
