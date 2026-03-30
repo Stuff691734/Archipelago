c10afdf3106d2a1cea236c80ebe6f17e3c9d6a16
Stuff691734 <23fuelln@gmail.com>
2026-03-30 00:08:11 -0500

    fixes for 1.20

--- src/main/java/net/stuff691734/archipelago/mixin/AdvancementVisibilityEvaluatorMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementVisibilityEvaluatorMixin.java
@@ -1,7 +1,7 @@
 
 package net.stuff691734.archipelago.mixin;
 
-import net.minecraft.advancements.AdvancementNode;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.server.advancements.AdvancementVisibilityEvaluator;
 import net.stuff691734.archipelago.Archipelago;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,19 +12,19 @@ import java.util.function.Predicate;
 
 @Mixin(AdvancementVisibilityEvaluator.class)
 public class AdvancementVisibilityEvaluatorMixin {
-    @Redirect(method = "evaluateVisibility(Lnet/minecraft/advancements/AdvancementNode;Lit/unimi/dsi/fastutil/Stack;Ljava/util/function/Predicate;Lnet/minecraft/server/advancements/AdvancementVisibilityEvaluator$Output;)Z", at = @At(value = "INVOKE", target = "Ljava/util/function/Predicate;test(Ljava/lang/Object;)Z"))
-    private static boolean alwaysDisplay(Predicate<AdvancementNode> instance, Object t) {
-        if (((AdvancementNode) t).advancement().display().isPresent()) {
+    @Redirect(method = "evaluateVisibility(Lnet/minecraft/advancements/Advancement;Lit/unimi/dsi/fastutil/Stack;Ljava/util/function/Predicate;Lnet/minecraft/server/advancements/AdvancementVisibilityEvaluator$Output;)Z", at = @At(value = "INVOKE", target = "Ljava/util/function/Predicate;test(Ljava/lang/Object;)Z"))
+    private static boolean alwaysDisplay(Predicate<Advancement> instance, Object t) {
+        if (((Advancement) t).getDisplay() != null) {
             if (Archipelago.slotData.isInitiated &&
                 (
                     !Archipelago.slotData.activated_modules.contains("Advancements") ||
-                    !Archipelago.slotData.advancement_difficulty.contains(((AdvancementNode) t).advancement().display().get().getFrame().getName())
+                    !Archipelago.slotData.advancement_difficulty.contains(((Advancement) t).getDisplay().getFrame().getName())
                 )
             ) {
-                return instance.test((AdvancementNode) t);
+                return instance.test((Advancement) t);
             }
             return true;
         }
-        return instance.test((AdvancementNode) t);
+        return instance.test((Advancement) t);
     }
 }
