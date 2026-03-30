0b75ec40922733b3bd322c37d4441e0a6748982b
Stuff691734 <23fuelln@gmail.com>
2026-03-30 13:58:10 -0500

    when getting checks update visible advancements

--- src/main/java/net/stuff691734/archipelago/events/archipealgo/ReceiveItemEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/ReceiveItemEvent.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.ModList;
 import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.Utils;
 import net.stuff691734.archipelago.ftbquests.FTBUtils;
+import net.stuff691734.archipelago.mixin.PlayerAdvancementAccessor;
 
 import javax.annotation.Nullable;
 
@@ -67,6 +68,8 @@ public class ReceiveItemEvent {
                 if (Utils.isAdvancementId(itemName)) {
                     Archipelago.archipelagoPersistentState.advancementChecks.put(itemName, true);
                     Advancement advancement = Archipelago.server.getAdvancements().getAdvancement(ResourceLocation.parse(itemName));
+                    PlayerAdvancementAccessor playerAdvancements = ((PlayerAdvancementAccessor)Archipelago.server.getPlayerList().getPlayerAdvancements(player));
+                    playerAdvancements.archipelago$markForVisibilityUpdate(advancement);
                     if (Archipelago.slotData.isInitiated && Archipelago.slotData.advancement_checks_give_items) {
                         assert advancement != null; // via isAdvancementId
                         DisplayInfo display = advancement.getDisplay();
