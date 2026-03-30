5129354394ff58ee725bd0058521c30067226cfe
Stuff691734 <23fuelln@gmail.com>
2026-03-29 23:38:13 -0500

    only show advancements that the player can complete and show all advancements the player can complete

---
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementWidgetMixin.java
@@ -0,0 +1,35 @@
+package net.stuff691734.archipelago.mixin;
+
+import net.minecraft.advancements.AdvancementNode;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.client.gui.screens.advancements.AdvancementWidget;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.Utils;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import javax.annotation.Nullable;
+
+@Mixin(AdvancementWidget.class)
+public abstract class AdvancementWidgetMixin {
+    @Shadow
+    @Nullable
+    private AdvancementWidget parent;
+
+    @Shadow
+    @Final
+    private DisplayInfo display;
+
+    @Shadow
+    @Final
+    private AdvancementNode advancementNode;
+
+    @Redirect(method = "drawConnectivity", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screens/advancements/AdvancementWidget;parent:Lnet/minecraft/client/gui/screens/advancements/AdvancementWidget;", opcode = Opcodes.GETFIELD))
+    public AdvancementWidget parent(AdvancementWidget thisWidget) {
+        return Utils.shouldAdvancementBeHidden(this.display, this.advancementNode) ? null : this.parent;
+    }
+}
