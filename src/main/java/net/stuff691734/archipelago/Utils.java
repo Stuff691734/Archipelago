bd5b6b82d972b5a819abc2bc6390dbb3fad59650
Stuff691734 <23fuelln@gmail.com>
2026-03-29 23:38:13 -0500

    only show advancements that the player can complete and show all advancements the player can complete

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -1,6 +1,7 @@
 package net.stuff691734.archipelago;
 
 import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -10,7 +11,7 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Objects;
 
 public class Utils {
     public static boolean isAdvancementId(String advancementId) {
@@ -70,6 +71,59 @@ public class Utils {
 
         for(ServerPlayerEntity player : Archipelago.server.getPlayerList().getPlayers()) {
             player.sendMessage(message, UUID.randomUUID());
+        }
+    }
+
+    public static boolean shouldAdvancementBeHidden(DisplayInfo display, AdvancementNode advancement) {
+        if (
+            Archipelago.slotData.isInitiated &&
+            (
+                !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
+            )
+        ) {
+            if (
+                Archipelago.slotData.activated_modules.contains("Advancements") &&
+                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
+            ) {
+                return true;
+            }
+
+            return display.isHidden();
+        }
+
+        if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
+            AdvancementNode rootAdvancement = AdvancementNode.getRoot(advancement);
+            String rootAdvancementName = rootAdvancement.holder().id().toString();
+
+            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(rootAdvancementName, false);
+        }
+        // parent advancement
+        else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
+            if (AdvancementNode.getRoot(advancement) == advancement) {
+                // if root check against self
+                return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
+            } else {
+                // otherwise check against values up tree not including self
+                AdvancementNode checkAdvancement = advancement;
+                // exits when all advancements up the tree have been checked
+                while (checkAdvancement != null) {
+                    checkAdvancement = checkAdvancement.parent();
+
+                    if (checkAdvancement != null) {
+                        String checkAdvancementName = checkAdvancement.holder().id().toString();
+                        if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkAdvancementName, false)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        // not either tab or tree... invalid/notstarted, going to check against self as I eventually want
+        // to do an advancement insanity thing
+        else {
+            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
         }
     }
 }
