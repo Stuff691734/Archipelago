9472529330953b915c14d1c25a843be26fcbd10f
Stuff691734 <23fuelln@gmail.com>
2026-03-30 14:19:50 -0500

    fixes for 1.20.1

--- src/main/java/net/stuff691734/archipelago/mixin/PlayerAdvancementAccessor.java
+++ src/main/java/net/stuff691734/archipelago/mixin/PlayerAdvancementAccessor.java
@@ -1,6 +1,6 @@
 package net.stuff691734.archipelago.mixin;
 
-import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.server.PlayerAdvancements;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -8,5 +8,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(PlayerAdvancements.class)
 public interface PlayerAdvancementAccessor {
     @Invoker("markForVisibilityUpdate")
-    void archipelago$markForVisibilityUpdate(AdvancementHolder advancement);
+    void archipelago$markForVisibilityUpdate(Advancement advancement);
 }
