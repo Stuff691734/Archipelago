dc25241f0592addbd8c0bf451a004beb94705914
Stuff691734 <23fuelln@gmail.com>
2026-03-31 19:02:39 -0500

    updates from version 2.0 to 2.2.6, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/ReceiveItemEvent.java
@@ -0,0 +1,93 @@
+package net.stuff691734.archipelago.events.archipealgo;
+
+import io.github.archipelagomw.events.ArchipelagoEventListener;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.fml.ModList;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.Utils;
+import net.stuff691734.archipelago.ftbquests.FTBUtils;
+import net.stuff691734.archipelago.mixin.PlayerAdvancementAccessor;
+
+import javax.annotation.Nullable;
+
+public class ReceiveItemEvent {
+    @ArchipelagoEventListener
+    public void onReceiveItems(io.github.archipelagomw.events.ReceiveItemEvent event) {
+        if (Archipelago.server != null) {
+            Utils.sendMessage(new StringTextComponent(String.format(
+                    "Received %s from %s (%s)",
+                    event.getItemName(),
+                    event.getPlayerName(),
+                    event.getLocationName()
+            )));
+            String[] itemName = event.getItemName().split(" ",2);
+
+            ReceiveItemEvent.parseItem(itemName[0], itemName[1], event.getIndex());
+        }
+    }
+
+    public static void parseItem(String itemType, String itemName, @Nullable Long index) {
+        for (ServerPlayerEntity player : Archipelago.server.getPlayerList().getPlayers()) {
+            playerParseItem(player, itemType, itemName, index);
+        }
+        switch (itemType) {
+            case "adv":
+                if (Utils.isAdvancementId(itemName)) {
+                    Archipelago.archipelagoPersistentState.advancementChecks.put(itemName, true);
+                }
+                break;
+            case "ftb":
+                if (ModList.get().isLoaded("ftbquests")) {
+                    if (FTBUtils.isQuestId(itemName)) {
+                        Archipelago.archipelagoPersistentState.ftbQuestChecks.put(itemName, true);
+                    }
+                }
+                break;
+        }
+        Archipelago.archipelagoPersistentState.setDirty();
+    }
+
+    public static void playerParseItem(ServerPlayerEntity player, String itemType, String itemName, @Nullable Long index) {
+        if (
+            index != null &&
+            Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(player.getStringUUID(),0) >= index
+        ) {
+            return;
+        }
+        if (index != null) {
+            Archipelago.archipelagoPersistentState.playerLastCheck.put(player.getStringUUID(), index.intValue());
+        }
+
+        switch (itemType) {
+            case "adv":
+                if (Utils.isAdvancementId(itemName)) {
+                    Archipelago.archipelagoPersistentState.advancementChecks.put(itemName, true);
+                    Advancement advancement = Archipelago.server.getAdvancements().getAdvancement(new ResourceLocation(itemName));
+                    PlayerAdvancementAccessor playerAdvancements = ((PlayerAdvancementAccessor)Archipelago.server.getPlayerList().getPlayerAdvancements(player));
+                    playerAdvancements.archipelago$markForVisibilityUpdate(advancement);
+                    if (Archipelago.slotData.isInitiated && Archipelago.slotData.advancement_checks_give_items) {
+                        assert advancement != null; // via isAdvancementId
+                        DisplayInfo display = advancement.getDisplay();
+                        if (display != null) {
+                            Utils.giveItem(player, display.getIcon().toString());
+                        }
+                    }
+                }
+                break;
+            case "ftb":
+                if (ModList.get().isLoaded("ftbquests") && FTBUtils.isQuestId(itemName)) {
+                    Archipelago.archipelagoPersistentState.ftbQuestChecks.put(itemName, true);
+                }
+                break;
+            case "item":
+                if (Utils.isItemId(itemName)) {
+                    Utils.giveItem(player, itemName);
+                }
+                break;
+        }
+    }
+}
