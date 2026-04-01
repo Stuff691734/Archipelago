e74a8b2cf9c82297e8bf3a78b6327e13cc86d34d
Stuff691734 <23fuelln@gmail.com>
2026-03-31 19:02:39 -0500

    updates from version 2.0 to 2.2.6, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/ServerStartingEvent.java
@@ -0,0 +1,41 @@
+package net.stuff691734.archipelago.events.neoforge;
+
+import io.github.archipelagomw.flags.ItemsHandling;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ArchipelagoClient;
+import net.stuff691734.archipelago.ArchipelagoPersistentState;
+import net.stuff691734.archipelago.SlotData;
+import net.stuff691734.archipelago.events.archipealgo.ArchipelagoEvents;
+
+public class ServerStartingEvent {
+    @SubscribeEvent
+    public void onEvent(FMLServerStartingEvent event) {
+        Archipelago.server = event.getServer();
+        ArchipelagoClient client = new ArchipelagoClient();
+
+        client.setGame("Modded Minecraft");
+
+        client.setItemsHandlingFlags(ItemsHandling.SEND_STARTING_INVENTORY | ItemsHandling.SEND_OWN_ITEMS | ItemsHandling.SEND_ITEMS);
+
+        ArchipelagoEvents.register(client.getEventManager());
+
+        Archipelago.client = client;
+
+        Archipelago.archipelagoPersistentState = ArchipelagoPersistentState.getServerState(event.getServer());
+
+        if (!Archipelago.archipelagoPersistentState.slotData.isEmpty()) {
+            Archipelago.slotData = new SlotData(
+                    Archipelago.archipelagoPersistentState.slotData.get("unlock_type"),
+                    Archipelago.archipelagoPersistentState.slotData.get("final_goal"),
+                    Archipelago.archipelagoPersistentState.slotData.get("activated_modules"),
+                    Archipelago.archipelagoPersistentState.slotData.get("advancement_check_difficulty"),
+                    Archipelago.archipelagoPersistentState.slotData.get("ftb_quest_check_shape"),
+                    Archipelago.archipelagoPersistentState.slotData.get("advancement_checks_give_items"),
+                    Archipelago.archipelagoPersistentState.slotData.get("quest_checks_give_rewards"),
+                    Archipelago.archipelagoPersistentState.slotData.get("death_link")
+            );
+        }
+    }
+}
--- src/main/java/net/stuff691734/archipelago/mixin/AdvancementMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementMixin.java
@@ -1,21 +1,28 @@
 package net.stuff691734.archipelago.mixin;
 
 
+import com.google.gson.stream.JsonReader;
+import com.mojang.serialization.Dynamic;
 import io.github.archipelagomw.ClientStatus;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.advancements.PlayerAdvancements;
+import net.minecraft.util.ResourceLocation;
 import net.stuff691734.archipelago.Archipelago;
-import net.stuff691734.archipelago.ChecksState;
 import net.stuff691734.archipelago.Utils;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Mixin(PlayerAdvancements.class)
 public abstract class AdvancementMixin {
@@ -23,21 +30,33 @@ public abstract class AdvancementMixin {
     @Shadow
     public abstract AdvancementProgress getProgress(Advancement advancement);
 
+    @Shadow
+    @Final
+    private Set<Advancement> progressChanged;
+
+    @Shadow
+    protected abstract void ensureVisibility(Advancement p_136011_);
+
+    @Shadow
+    protected abstract void startProgress(Advancement p_135986_, AdvancementProgress p_135987_);
+
     @Inject(
             method = "grantCriterion",
             at = @At("RETURN")
     )
     private void sendArchipelagoAdvancement(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
-        if (advancement.getDisplay() != null && this.getProgress(advancement).isDone()) {
+        if (this.getProgress(advancement).isDone()) {
             if (Archipelago.client.isConnected()) {
-                Long advancement_id = Archipelago.client.getDataPackage().getGame("Modded Minecraft").locationNameToId.get(advancement.getId().toString());
+                Long advancement_id = Archipelago.client.getDataPackage().getGame("Modded Minecraft").locationNameToId.get("adv " + advancement.getId());
                 if (advancement_id != null) {
                     Archipelago.client.getLocationManager().checkLocation(advancement_id);
-                    ChecksState checksState = ChecksState.getServerState(Archipelago.server);
-                    if (advancement.getId().toString().equals(checksState.slotData.get("final_goal"))) {
+                    if (("adv " + advancement.getId()).equals(Archipelago.slotData.final_goal)) {
                         Archipelago.client.setGameState(ClientStatus.CLIENT_GOAL);
                     }
                 }
+            } else {
+                Archipelago.archipelagoPersistentState.pendingChecks.add("adv " + advancement.getId());
+                Archipelago.archipelagoPersistentState.setDirty();
             }
         }
     }
@@ -50,22 +69,30 @@ public abstract class AdvancementMixin {
     private void preventAdvancement(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
         DisplayInfo display = advancement.getDisplay();
         if (display != null) {
-            ChecksState checksState = ChecksState.getServerState(Archipelago.server);
+            if (
+                Archipelago.slotData.isInitiated &&
+                (
+                    !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                    !Archipelago.slotData.advancement_difficulty.contains(display.getFrame().getName())
+                )
+            ) {
+                return;
+            }
 
-            if (Objects.equals(checksState.slotData.get("unlock_type"), "tab")) {
+            if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
                 Advancement rootAdvancement = Utils.getRoot(advancement);
                 String rootAdvancementName = rootAdvancement.getId().toString();
 
-                if (!checksState.checks.getOrDefault(rootAdvancementName, false)) {
+                if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(rootAdvancementName, false)) {
                     // if player hasn't received root check prevent them from getting the advancement
                     cir.setReturnValue(false);
                 }
             }
             // parent advancement
-            else if (Objects.equals(checksState.slotData.get("unlock_type"), "tree")) {
+            else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
                 if (Utils.getRoot(advancement) == advancement) {
                     // if root check against self
-                    if (!checksState.checks.getOrDefault(advancement.getId().toString(), false)) {
+                    if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.getId().toString(), false)) {
                         cir.setReturnValue(false);
                     }
                 } else {
@@ -77,7 +104,7 @@ public abstract class AdvancementMixin {
 
                         if (checkAdvancement != null) {
                             String checkAdvancementName = checkAdvancement.getId().toString();
-                            if (!checksState.checks.getOrDefault(checkAdvancementName, false)) {
+                            if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkAdvancementName, false)) {
                                 cir.setReturnValue(false);
                             }
                         }
@@ -87,8 +114,43 @@ public abstract class AdvancementMixin {
             // not either tab or tree... invalid/notstarted, going to check against self as I eventually want
             // to do an advancement insanity thing
             else {
-                cir.setReturnValue(checksState.checks.getOrDefault(advancement.getId().toString(), false));
+                cir.setReturnValue(Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.getId().toString(), false));
             }
         }
+    }
+
+    @Inject(method = "shouldBeVisible", at = @At(value = "HEAD"), cancellable = true)
+    public void shouldBeVisible(Advancement advancement, CallbackInfoReturnable<Boolean> cir) {
+        if (advancement.getDisplay() != null) {
+            if (Archipelago.slotData.isInitiated &&
+                (
+                    !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                    !Archipelago.slotData.advancement_difficulty.contains(advancement.getDisplay().getFrame().getName())
+                )
+            ) {
+                return;
+            }
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Redirect(method = "load", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;collect(Ljava/util/stream/Collector;)Ljava/lang/Object;"))
+    private Object forEach(Stream<Map.Entry<ResourceLocation, AdvancementProgress>> instance, Collector<Map.Entry<ResourceLocation, AdvancementProgress>, ?, List<Map.Entry<ResourceLocation, AdvancementProgress>>> arCollector) {
+        Map<ResourceLocation, AdvancementProgress> list = Archipelago.server.getAdvancements().getAllAdvancements().stream().map(
+            (advancement) -> new AbstractMap.SimpleImmutableEntry<>(advancement.getId(), this.getOrStartProgress(advancement))).collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (e1, e2)->e1
+            )
+        );
+        instance.forEach((entry) -> list.put(entry.getKey(), entry.getValue()));
+        return new ArrayList<>(list.entrySet());
+    }
+
+    @Redirect(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/PlayerAdvancements;startProgress(Lnet/minecraft/advancements/Advancement;Lnet/minecraft/advancements/AdvancementProgress;)V"))
+    public void showThings(PlayerAdvancements instance, Advancement advancement, AdvancementProgress progress) {
+        this.startProgress(advancement, progress);
+        this.progressChanged.add(advancement);
+        this.ensureVisibility(advancement);
     }
 }
