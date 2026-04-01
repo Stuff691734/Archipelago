fad2a708a34fc78f896981206105d44ee3b5e815
Stuff691734 <23fuelln@gmail.com>
2026-03-31 20:58:28 -0500

    changes to work with 1.16.3

--- src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/gui/quests/RewardButtonMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/gui/quests/RewardButtonMixin.java
@@ -1,6 +1,6 @@
 package net.stuff691734.archipelago.mixin.FTBQuests.gui.quests;
 
-import com.feed_the_beast.ftbquests.gui.quests.RewardButton;
+import com.feed_the_beast.ftbquests.gui.quests.ButtonReward;
 import com.feed_the_beast.ftbquests.quest.PlayerData;
 import com.feed_the_beast.ftbquests.quest.QuestObject;
 import net.stuff691734.archipelago.ftbquests.FTBUtils;
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RewardButton.class)
+@Mixin(ButtonReward.class)
 public class RewardButtonMixin {
     @Redirect(method = "draw", at = @At(value = "INVOKE", target = "Lcom/feed_the_beast/ftbquests/quest/PlayerData;isComplete(Lcom/feed_the_beast/ftbquests/quest/QuestObject;)Z"), remap = false)
     private boolean modifyRewardAccess(PlayerData playerData, QuestObject questObject) {
