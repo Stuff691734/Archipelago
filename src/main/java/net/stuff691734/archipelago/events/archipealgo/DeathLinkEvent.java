d1fa355d819223dd517a9c1c1387fceb5965b4e5
Stuff691734 <23fuelln@gmail.com>
2026-03-31 19:02:39 -0500

    updates from version 2.0 to 2.2.6, see branch neoforge-1.21.1 for full details

---
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/DeathLinkEvent.java
@@ -0,0 +1,17 @@
+package net.stuff691734.archipelago.events.archipealgo;
+
+import io.github.archipelagomw.events.ArchipelagoEventListener;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.Utils;
+
+public class DeathLinkEvent {
+    @ArchipelagoEventListener
+    public void onDeathLink(io.github.archipelagomw.events.DeathLinkEvent event) {
+        Utils.sendMessage(new StringTextComponent(String.format("[DeathLink] %s died: %s",event.source, event.cause)));
+        for (ServerPlayerEntity player : Archipelago.server.getPlayerList().getPlayers()) {
+            player.hurt(Archipelago.DeathLinkDamage, Float.MAX_VALUE);
+        }
+    }
+}
