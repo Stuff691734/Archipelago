5b684d272f0de7025cb5f6d790afefc127782347
Stuff691734 <23fuelln@gmail.com>
2026-04-03 17:38:21 -0500

    fixes for 1.15.1, switch mixins to js coremods

--- src/main/java/net/stuff691734/archipelago/mixin/AdvancementWidgetMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementWidgetMixin.java
@@ -1,34 +1,26 @@
-package net.stuff691734.archipelago.mixin;
-
-import net.minecraft.advancements.Advancement;
-import net.minecraft.advancements.DisplayInfo;
-import net.minecraft.client.gui.advancements.AdvancementEntryGui;
-import net.stuff691734.archipelago.Utils;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import javax.annotation.Nullable;
-
-@Mixin(AdvancementEntryGui.class)
-public abstract class AdvancementWidgetMixin {
-    @Shadow
-    @Nullable
-    private AdvancementEntryGui parent;
-
-    @Shadow
-    @Final
-    private DisplayInfo displayInfo;
-
-    @Shadow
-    @Final
-    private Advancement advancement;
-
-    @Redirect(method = "drawConnectionLineToParent", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/advancements/AdvancementEntryGui;parent:Lnet/minecraft/client/gui/advancements/AdvancementEntryGui;", opcode = Opcodes.GETFIELD))
-    public AdvancementEntryGui parent(AdvancementEntryGui thisWidget) {
-        return Utils.shouldAdvancementBeHidden(this.displayInfo, this.advancement) ? null : this.parent;
-    }
-}
+//package net.stuff691734.archipelago.mixin;
+//
+//import net.minecraft.advancements.Advancement;
+//import net.minecraft.advancements.DisplayInfo;
+//import net.minecraft.client.gui.advancements.AdvancementEntryGui;
+//import net.stuff691734.archipelago.Utils;
+//
+////@Mixin(AdvancementEntryGui.class)
+//public abstract class AdvancementWidgetMixin {
+////    @Shadow
+////    @Nullable
+////    private AdvancementEntryGui parent;
+////
+////    @Shadow
+////    @Final
+////    private DisplayInfo displayInfo;
+////
+////    @Shadow
+////    @Final
+////    private Advancement advancement;
+////
+////    @Redirect(method = "drawConnectionLineToParent", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/advancements/AdvancementEntryGui;parent:Lnet/minecraft/client/gui/advancements/AdvancementEntryGui;", opcode = Opcodes.GETFIELD))
+//    public AdvancementEntryGui parent(AdvancementEntryGui thisWidget) {
+//        return Utils.shouldAdvancementBeHidden(thisWidget.displayInfo, thisWidget.advancement) ? null : thisWidget.parent;
+//    }
+//}
--- src/main/java/net/stuff691734/archipelago/mixin/DisplayInfoMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/DisplayInfoMixin.java
@@ -1,28 +1,28 @@
-package net.stuff691734.archipelago.mixin;
-
-import net.minecraft.advancements.Advancement;
-import net.minecraft.advancements.DisplayInfo;
-import net.minecraft.client.gui.advancements.AdvancementEntryGui;
-import net.stuff691734.archipelago.Utils;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-@Mixin(AdvancementEntryGui.class)
-public class DisplayInfoMixin {
-    @Shadow
-    @Final
-    private Advancement advancement;
-
-    @Redirect(method = "drawAdvancement", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
-    public boolean drawSetNotHidden(DisplayInfo display) {
-        return Utils.shouldAdvancementBeHidden(display, this.advancement);
-    }
-
-    @Redirect(method = "isMouseOver", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
-    public boolean isMouseOverSetNotHidden(DisplayInfo display) {
-        return Utils.shouldAdvancementBeHidden(display, this.advancement);
-    }
-}
+//package net.stuff691734.archipelago.mixin;
+//
+//import net.minecraft.advancements.Advancement;
+//import net.minecraft.advancements.DisplayInfo;
+//import net.minecraft.client.gui.advancements.AdvancementEntryGui;
+//import net.stuff691734.archipelago.Utils;
+////import org.spongepowered.asm.mixin.Final;
+////import org.spongepowered.asm.mixin.Mixin;
+////import org.spongepowered.asm.mixin.Shadow;
+////import org.spongepowered.asm.mixin.injection.At;
+////import org.spongepowered.asm.mixin.injection.Redirect;
+//
+////@Mixin(AdvancementEntryGui.class)
+//public class DisplayInfoMixin {
+////    @Shadow
+////    @Final
+////    private Advancement advancement;
+////
+////    @Redirect(method = "drawAdvancement", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
+//    public boolean drawSetNotHidden(DisplayInfo display) {
+//        return Utils.shouldAdvancementBeHidden(display, this.advancement);
+//    }
+//
+////    @Redirect(method = "isMouseOver", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
+//    public boolean isMouseOverSetNotHidden(DisplayInfo display) {
+//        return Utils.shouldAdvancementBeHidden(display, this.advancement);
+//    }
+//}
