ff26596acfcd29f4f5c2af3267166ee9643f7b5c
Stuff691734 <23fuelln@gmail.com>
2026-04-04 11:43:40 -0500

    fixes for 1.13.2

--- src/main/java/net/stuff691734/archipelago/events/archipealgo/DeathLinkEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/DeathLinkEvent.java
@@ -1,16 +1,16 @@
 package net.stuff691734.archipelago.events.archipealgo;
 
 import io.github.archipelagomw.events.ArchipelagoEventListener;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
 import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.Utils;
 
 public class DeathLinkEvent {
     @ArchipelagoEventListener
     public void onDeathLink(io.github.archipelagomw.events.DeathLinkEvent event) {
-        Utils.sendMessage(new StringTextComponent(String.format("[DeathLink] %s died: %s",event.source, event.cause)));
-        for (ServerPlayerEntity player : Archipelago.server.getPlayerList().getPlayers()) {
+        Utils.sendMessage(new TextComponentString(String.format("[DeathLink] %s died: %s",event.source, event.cause)));
+        for (EntityPlayerMP player : Archipelago.server.getPlayerList().getPlayers()) {
             player.attackEntityFrom(Archipelago.DeathLinkDamage, Float.MAX_VALUE);
         }
     }
