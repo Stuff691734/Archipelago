0713b09f1cdd1cae759e577592dddfc92de2c5fe
Stuff691734 <23fuelln@gmail.com>
2026-04-04 01:10:23 -0500

    fixes for 1.14.3

--- src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
@@ -2,8 +2,8 @@ package net.stuff691734.archipelago.events.neoforge;
 
 import io.github.archipelagomw.parts.NetworkItem;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.events.archipealgo.ReceiveItemEvent;
 
@@ -11,13 +11,13 @@ public class EntityLoadEvent {
     @SubscribeEvent
     public void onEvent(PlayerEvent.PlayerLoggedInEvent event) {
         int serverLastCheck = Archipelago.client.getItemManager().getIndex();
-        int playerLastCheck = Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(event.getEntity().getCachedUniqueIdString(), 0);
+        int playerLastCheck = Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(event.getPlayer().getCachedUniqueIdString(), 0);
         if (serverLastCheck > playerLastCheck) {
-            Archipelago.archipelagoPersistentState.playerLastCheck.put(event.getEntity().getCachedUniqueIdString(), serverLastCheck);
+            Archipelago.archipelagoPersistentState.playerLastCheck.put(event.getPlayer().getCachedUniqueIdString(), serverLastCheck);
 
             for (NetworkItem item: Archipelago.client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
                 String[] itemName = item.itemName.split(" ",2);
-                ReceiveItemEvent.playerParseItem((ServerPlayerEntity) event.getEntity(), itemName[0], itemName[1], null);
+                ReceiveItemEvent.playerParseItem((ServerPlayerEntity) event.getPlayer(), itemName[0], itemName[1], null);
             }
             Archipelago.archipelagoPersistentState.setDirty(true);
         }
