94d273eb319aff16faefc6a89b3660016acf9c1f
Stuff691734 <23fuelln@gmail.com>
2026-04-06 23:46:14 -0500

    ServerPlayer -> ServerPlayerEntity

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -53,11 +53,11 @@ public class Utils {
         }
     }
 
-    public static void giveItem(ServerPlayer player, Item item) {
+    public static void giveItem(ServerPlayerEntity player, Item item) {
         giveItem(player, item, 1);
     }
 
-    public static void giveItem(ServerPlayer player, Item item, int amount) {
+    public static void giveItem(ServerPlayerEntity player, Item item, int amount) {
         ItemStack itemStack = new ItemStack(item, amount);
         if (!player.inventory.addItemStackToInventory(itemStack)) {
             player.entityDropItem(itemStack);
