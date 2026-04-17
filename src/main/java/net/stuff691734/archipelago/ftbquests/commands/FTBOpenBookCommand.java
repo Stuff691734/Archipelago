a3a421d1bb5ce9e704dd6a7ad8ba2dda4541b65e
Stuff691734 <23fuelln@gmail.com>
2026-04-17 16:13:36 -0500

    recreate /ftbquests open_book as /archipelago open_book fixes for 1.16.5

--- src/main/java/net/stuff691734/archipelago/ftbquests/commands/FTBOpenBookCommand.java
+++ src/main/java/net/stuff691734/archipelago/ftbquests/commands/FTBOpenBookCommand.java
@@ -2,21 +2,21 @@ package net.stuff691734.archipelago.ftbquests.commands;
 
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.PacketDistributor;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraftforge.fml.network.PacketDistributor;
 import net.stuff691734.archipelago.ArchipelagoPacketHandler;
 import net.stuff691734.archipelago.ftbquests.net.OpenQuestBookPacket;
 
 public class FTBOpenBookCommand {
-    public static int execute(CommandContext<CommandSourceStack> context, String quest_id) throws CommandSyntaxException {
+    public static int execute(CommandContext<CommandSource> context, String quest_id) throws CommandSyntaxException {
         long quest;
         try {
             quest = Long.parseLong(quest_id, 16);
         } catch (NumberFormatException ex) {
             quest = 0L;
         }
-        ServerPlayer player = context.getSource().getPlayerOrException();
+        ServerPlayerEntity player = context.getSource().getPlayerOrException();
         ArchipelagoPacketHandler.INSTANCE.send(
                 PacketDistributor.PLAYER.with(() -> player),
                 new OpenQuestBookPacket(quest)
