3ed0c231edad03adca4a82bd79434f62b68eb598
Stuff691734 <23fuelln@gmail.com>
2026-04-17 16:13:36 -0500

    recreate /ftbquests open_book as /archipelago open_book fixes for 1.16.5

--- src/main/java/net/stuff691734/archipelago/ArchipelagoPacketHandler.java
+++ src/main/java/net/stuff691734/archipelago/ArchipelagoPacketHandler.java
@@ -1,9 +1,9 @@
 package net.stuff691734.archipelago;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.network.NetworkDirection;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkDirection;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 import net.stuff691734.archipelago.ftbquests.net.OpenQuestBookPacket;
 
 public class ArchipelagoPacketHandler {
