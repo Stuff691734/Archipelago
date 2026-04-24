99c875cb0aba449a3fd8bc9efd6c31e19b61988c
Stuff691734 <23fuelln@gmail.com>
2026-04-23 23:02:46 -0500

    prevent crash from LocalRandomSource

--- src/main/java/net/stuff691734/archipelago/events/archipealgo/DeathLinkEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/DeathLinkEvent.java
@@ -11,8 +11,10 @@ public class DeathLinkEvent {
     @ArchipelagoEventListener
     public void onDeathLink(io.github.archipelagomw.events.DeathLinkEvent event) {
         Utils.sendMessage(new TextComponent(String.format("[DeathLink] %s died: %s",event.source, event.cause)));
-        for (ServerPlayer player : Archipelago.server.getPlayerList().getPlayers()) {
-            player.hurt(Archipelago.DeathLinkDamage, Float.MAX_VALUE);
-        }
+        Archipelago.server.execute(() -> {
+            for (ServerPlayer player : Archipelago.server.getPlayerList().getPlayers()) {
+                player.hurt(Archipelago.DeathLinkDamage, Float.MAX_VALUE);
+            }
+        });
     }
 }
