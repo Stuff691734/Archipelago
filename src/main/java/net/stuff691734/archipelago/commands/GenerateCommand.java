0a6880edbd355b9dc668d498a04691d2cbd5bec4
Stuff691734 <23fuelln@gmail.com>
2026-04-24 20:05:02 -0500

    add option when generating to remove quests that are permanently hidden (parity, no difference without quest mod)

--- src/main/java/net/stuff691734/archipelago/commands/GenerateCommand.java
+++ src/main/java/net/stuff691734/archipelago/commands/GenerateCommand.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class GenerateCommand {
-    public static int execute(CommandContext<CommandSourceStack> context, boolean singleLine) {
+    public static int execute(CommandContext<CommandSourceStack> context, boolean singleLine, boolean removePermaHidden) {
         context.getSource().sendSuccess(new TextComponent("Started writing to file."), false);
 
         Map<String, Map<String, ? extends Check>> checks = new HashMap<>();
