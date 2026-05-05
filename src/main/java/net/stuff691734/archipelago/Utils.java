89de3b560d52f5cd1a9ab2ddd3f7e178e644b86f
Stuff691734 <23fuelln@gmail.com>
2026-05-04 21:47:33 -0500

    fix issue with crash on 1.12.2 server (client does not have access to server)

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -5,24 +5,28 @@ import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.ResourceLocationException;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 
-import java.util.UUID;
+import javax.annotation.Nullable;
 import java.util.Objects;
 
 public class Utils {
     public static boolean isAdvancementId(String advancementId) {
+        if (Archipelago.getServer() == null) {
+            return false;
+        }
         ResourceLocation id;
         try {
             id = new ResourceLocation(advancementId);
         } catch (ResourceLocationException exception) {
             return false;
         }
-        Advancement advancement = Archipelago.server.getAdvancementManager().getAdvancement(id);
+        Advancement advancement = Archipelago.getServer().getAdvancementManager().getAdvancement(id);
         return advancement != null;
     }
 
@@ -43,24 +47,38 @@ public class Utils {
         return ForgeRegistries.ITEMS.containsKey(id);
     }
 
-    public static void giveItem(ServerPlayerEntity player, String itemId) {
-        String[] strings = itemId.split(" ", 2);
-        int amount = Integer.parseInt(strings[0]);
-        String item = strings[1];
-        Item itemValue = ForgeRegistries.ITEMS.getValue(new ResourceLocation(item));
-        if (itemValue != null) {
-            giveItem(player, itemValue, amount);
-        }
-    }
-
-    public static void giveItem(ServerPlayerEntity player, Item item) {
-        giveItem(player, item, 1);
-    }
-
     public static void giveItem(ServerPlayerEntity player, Item item, int amount) {
         ItemStack itemStack = new ItemStack(item, amount);
         if (!player.inventory.addItemStackToInventory(itemStack)) {
             player.entityDropItem(itemStack);
+        }
+    }
+
+    public static void giveItem(MinecraftServer server, String item, @Nullable Long index) {
+        String[] strings = item.split(" ", 2);
+        int amount = Integer.parseInt(strings[0]);
+        String itemName = strings[1];
+        Item itemValue = ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName));
+        if (itemValue != null) {
+            giveItem(server, itemValue, amount, index);
+        }
+    }
+
+    public static void giveItem(MinecraftServer server, Item item, @Nullable Long index) {
+        giveItem(server, item, 1, index);
+    }
+
+    public static void giveItem(MinecraftServer server, Item item, int amount, @Nullable Long index) {
+        for (ServerPlayerEntity player : server.getPlayerList().getPlayers()) {
+            if (index != null) {
+                if (ArchipelagoPersistentState.getInstance() != null) {
+                    if (ArchipelagoPersistentState.getInstance().playerLastCheck.getOrDefault(player.getCachedUniqueIdString(), 0) < index) {
+                        giveItem(player, item, amount);
+                    }
+                }
+            } else {
+                giveItem(player, item, amount);
+            }
         }
     }
 
@@ -76,11 +94,13 @@ public class Utils {
     }
 
     public static void sendMessage(StringTextComponent message) {
-        Archipelago.server.sendMessage(message);
+        Archipelago.executeOnServer((server) -> {
+            server.sendMessage(message);
 
-        for(ServerPlayerEntity player : Archipelago.server.getPlayerList().getPlayers()) {
-            player.sendMessage(message);
-        }
+            for(ServerPlayerEntity player : server.getPlayerList().getPlayers()) {
+                player.sendMessage(message);
+            }
+        });
     }
 
     public static boolean shouldAdvancementBeHidden(DisplayInfo display, Advancement advancement) {
@@ -105,13 +125,13 @@ public class Utils {
             Advancement rootAdvancement = Utils.getRoot(advancement);
             String rootAdvancementName = rootAdvancement.getId().toString();
 
-            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(rootAdvancementName, false);
+            return !ArchipelagoPersistentState.getAdvancement(rootAdvancementName);
         }
         // parent advancement
         else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
             if (Utils.getRoot(advancement) == advancement) {
                 // if root check against self
-                return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.getId().toString(), false);
+                return !ArchipelagoPersistentState.getAdvancement(advancement.getId().toString());
             } else {
                 // otherwise check against values up tree not including self
                 Advancement checkAdvancement = advancement;
@@ -121,7 +141,7 @@ public class Utils {
 
                     if (checkAdvancement != null) {
                         String checkAdvancementName = checkAdvancement.getId().toString();
-                        if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkAdvancementName, false)) {
+                        if (!ArchipelagoPersistentState.getAdvancement(checkAdvancementName)) {
                             return true;
                         }
                     }
@@ -132,7 +152,7 @@ public class Utils {
         // not either tab or tree... invalid/notstarted, going to check against self as I eventually want
         // to do an advancement insanity thing
         else {
-            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.getId().toString(), false);
+            return !ArchipelagoPersistentState.getAdvancement(advancement.getId().toString());
         }
     }
 }
--- src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
@@ -1,25 +1,27 @@
 package net.stuff691734.archipelago.events.neoforge;
 
 import io.github.archipelagomw.parts.NetworkItem;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ArchipelagoPersistentState;
 import net.stuff691734.archipelago.events.archipealgo.ReceiveItemEvent;
 
 public class EntityLoadEvent {
     @SubscribeEvent
     public void onEvent(PlayerEvent.PlayerLoggedInEvent event) {
-        int serverLastCheck = Archipelago.client.getItemManager().getIndex();
-        int playerLastCheck = Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(event.getPlayer().getCachedUniqueIdString(), 0);
-        if (serverLastCheck > playerLastCheck) {
-            Archipelago.archipelagoPersistentState.playerLastCheck.put(event.getPlayer().getCachedUniqueIdString(), serverLastCheck);
+        if (Archipelago.getServer() != null && ArchipelagoPersistentState.getInstance() != null) {
+            int serverLastCheck = Archipelago.client.getItemManager().getIndex();
+            int playerLastCheck = ArchipelagoPersistentState.getInstance().playerLastCheck.getOrDefault(event.getPlayer().getCachedUniqueIdString(), 0);
+            if (serverLastCheck > playerLastCheck) {
+                ArchipelagoPersistentState.getInstance().playerLastCheck.put(event.getPlayer().getCachedUniqueIdString(), serverLastCheck);
 
-            for (NetworkItem item: Archipelago.client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
-                String[] itemName = item.itemName.split(" ",2);
-                ReceiveItemEvent.playerParseItem((ServerPlayerEntity) event.getPlayer(), itemName[0], itemName[1], null);
+                for (NetworkItem item : Archipelago.client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
+                    String[] itemName = item.itemName.split(" ", 2);
+                    ReceiveItemEvent.serverParseItem(Archipelago.getServer(), ArchipelagoPersistentState.getInstance(), itemName[0], itemName[1], null);
+                }
+                ArchipelagoPersistentState.getInstance().setDirty(true);
             }
-            Archipelago.archipelagoPersistentState.setDirty(true);
         }
     }
 }
