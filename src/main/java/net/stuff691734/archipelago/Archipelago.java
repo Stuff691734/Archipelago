c2a57ea38bafae844c95c3936f7aac6c35786534
Stuff691734 <23fuelln@gmail.com>
2026-05-06 23:13:44 -0500

    version 2.2.9

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -8,6 +8,9 @@ import net.stuff691734.archipelago.events.neoforge.ForgeEvents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+
 @Mod(Archipelago.MODID)
 public class Archipelago {
     public static final String MODID = "archipelago";
@@ -21,5 +24,19 @@ public class Archipelago {
 
     public Archipelago() {
         ForgeEvents.register(MinecraftForge.EVENT_BUS);
+    }
+
+    public static @Nullable MinecraftServer getServer() {
+        return server;
+    }
+
+    public static void setServer(MinecraftServer server) {
+        Archipelago.server = server;
+    }
+
+    public static void executeOnServer(Consumer<MinecraftServer> action) {
+        if (Archipelago.getServer() != null) {
+            action.accept(Archipelago.getServer());
+        }
     }
 }
