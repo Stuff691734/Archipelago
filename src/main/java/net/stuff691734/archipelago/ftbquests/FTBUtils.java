15ff0db4f73f7af7640d9eec1fb7603281d8abf0
Stuff691734 <23fuelln@gmail.com>
2026-05-06 22:22:29 -0500

    fix bug with flexible quest completion causing quests to be completed when they should not have been able to

--- src/main/java/net/stuff691734/archipelago/ftbquests/FTBUtils.java
+++ src/main/java/net/stuff691734/archipelago/ftbquests/FTBUtils.java
@@ -1,9 +1,13 @@
 package net.stuff691734.archipelago.ftbquests;
 
 import dev.ftb.mods.ftbquests.api.FTBQuestsAPI;
+import dev.ftb.mods.ftbquests.client.ClientQuestFile;
 import dev.ftb.mods.ftbquests.quest.Quest;
 import dev.ftb.mods.ftbquests.quest.QuestObject;
+import dev.ftb.mods.ftbquests.quest.TeamData;
+import net.minecraft.server.level.ServerPlayer;
 import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ftbquests.accessor.QuestAccessor;
 
 import java.util.function.Function;
 
@@ -37,4 +41,13 @@ public class FTBUtils {
         return action.apply(questObject);
     }
 
+    public static void checkIsCompleted(ServerPlayer player, String questName) {
+        // not doing this safely as it has already been checked that this works via FTBUtils.isQuestId()
+        long questId = Long.parseLong(questName, 16);
+        ClientQuestFile file = ClientQuestFile.INSTANCE;
+        QuestObject questObject = file.get(questId);
+        if (questObject instanceof Quest quest) {
+            ((QuestAccessor)(Object)quest).archipelago$checkForDependantCompletion(TeamData.get(player));
+        }
+    }
 }
