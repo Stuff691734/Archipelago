529aae6c28cd41703653d79e00032c4f6685f0cc
Stuff691734 <23fuelln@gmail.com>
2026-05-16 08:15:32 -0500

    change how generated data is output

--- src/main/java/net/stuff691734/archipelago/archipelagoData/FTBQuestsCheck.java
+++ src/main/java/net/stuff691734/archipelago/archipelagoData/FTBQuestsCheck.java
@@ -1,19 +1,13 @@
 package net.stuff691734.archipelago.archipelagoData;
 
 public class FTBQuestsCheck extends Check implements Comparable<FTBQuestsCheck> {
-    public String dependant_type;
-    public String chapter;
-    public String[] advancement_dependencies;
 
-    public FTBQuestsCheck(String type, String[] dependants, String dependant_type, String chapter, String[] advancement_dependencies) {
-        super(type, dependants);
-        this.dependant_type = dependant_type;
-        this.chapter = chapter;
-        this.advancement_dependencies = advancement_dependencies;
+    public FTBQuestsCheck(String type, DependencyNotation dependencies, String chapter) {
+        super(type, dependencies, chapter);
     }
 
     @Override
     public int compareTo(FTBQuestsCheck ftbQuestsCheck) {
-        return this.chapter.compareTo(ftbQuestsCheck.chapter);
+        return this.page.compareTo(ftbQuestsCheck.page);
     }
 }
