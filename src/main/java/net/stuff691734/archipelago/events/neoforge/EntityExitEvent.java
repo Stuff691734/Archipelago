9b761fa6dbd3d1a4fa4165110012dfbf1e6ced5a
Stuff691734 <23fuelln@gmail.com>
2026-05-17 15:04:22 -0500

    fixes for 1.14.4

--- src/main/java/net/stuff691734/archipelago/events/neoforge/EntityExitEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/EntityExitEvent.java
@@ -1,7 +1,7 @@
 package net.stuff691734.archipelago.events.neoforge;
 
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.stuff691734.archipelago.Archipelago;
 
 public class EntityExitEvent {
