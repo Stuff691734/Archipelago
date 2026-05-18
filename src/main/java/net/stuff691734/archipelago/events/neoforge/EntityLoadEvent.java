4ec9c15bb80d43e4752b4135aeacf372e6ab1cad
Stuff691734 <23fuelln@gmail.com>
2026-05-18 16:21:36 -0500

    changes for 1.15.2 with ftb logic. see 1.12.2 for more details.

--- src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/EntityLoadEvent.java
@@ -4,22 +4,39 @@ import io.github.archipelagomw.parts.NetworkItem;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.network.PacketDistributor;
 import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ArchipelagoPacketHandler;
+import net.stuff691734.archipelago.ArchipelagoPersistentState;
 import net.stuff691734.archipelago.events.archipealgo.ReceiveItemEvent;
+import net.stuff691734.archipelago.net.StartSyncChecksPacket;
+import net.stuff691734.archipelago.net.SyncSlotDataPacket;
 
 public class EntityLoadEvent {
     @SubscribeEvent
     public void onEvent(PlayerEvent.PlayerLoggedInEvent event) {
-        int serverLastCheck = Archipelago.client.getItemManager().getIndex();
-        int playerLastCheck = Archipelago.archipelagoPersistentState.playerLastCheck.getOrDefault(event.getEntity().getStringUUID(), 0);
-        if (serverLastCheck > playerLastCheck) {
-            Archipelago.archipelagoPersistentState.playerLastCheck.put(event.getEntity().getStringUUID(), serverLastCheck);
+        if (Archipelago.getServer() != null && ArchipelagoPersistentState.getInstance() != null) {
+            int serverLastCheck = Archipelago.client.getItemManager().getIndex();
+            int playerLastCheck = ArchipelagoPersistentState.getInstance().playerLastCheck.getOrDefault(event.getPlayer().getStringUUID(), 0);
+            if (serverLastCheck > playerLastCheck) {
+                ArchipelagoPersistentState.getInstance().playerLastCheck.put(event.getPlayer().getStringUUID(), serverLastCheck);
 
-            for (NetworkItem item: Archipelago.client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
-                String[] itemName = item.itemName.split(" ",2);
-                ReceiveItemEvent.playerParseItem((ServerPlayerEntity) event.getEntity(), itemName[0], itemName[1], null);
+                for (NetworkItem item : Archipelago.client.getItemManager().getReceivedItems().subList(playerLastCheck, serverLastCheck)) {
+                    String[] itemName = item.itemName.split(" ", 3);
+                    ReceiveItemEvent.serverParseItem(Archipelago.getServer(), ArchipelagoPersistentState.getInstance(), itemName[0], itemName[1], null);
+                }
+                ArchipelagoPersistentState.getInstance().setDirty();
             }
-            Archipelago.archipelagoPersistentState.setDirty();
+            ArchipelagoPacketHandler.INSTANCE.send(
+                PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) event.getPlayer()),
+                new StartSyncChecksPacket(ArchipelagoPersistentState.getInstance().checks.keySet().toArray(new String[0]))
+            );
+            if (!ArchipelagoPersistentState.getInstance().slotData.isEmpty()) {
+                ArchipelagoPacketHandler.INSTANCE.send(
+                    PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity) event.getPlayer()),
+                    new SyncSlotDataPacket(ArchipelagoPersistentState.getInstance().slotData)
+                );
+            }
         }
     }
 }
