f4d19ce0889a4c6eaddb52f355034a8ef3b315d2
Stuff691734 <23fuelln@gmail.com>
2026-05-18 16:21:36 -0500

    changes for 1.15.2 with ftb logic. see 1.12.2 for more details.

--- src/main/java/net/stuff691734/archipelago/mixin/PlayerAdvancementAccessor.java
+++ src/main/java/net/stuff691734/archipelago/mixin/PlayerAdvancementAccessor.java
@@ -8,5 +8,5 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 @Mixin(PlayerAdvancements.class)
 public interface PlayerAdvancementAccessor {
     @Invoker("ensureVisibility")
-    void archipelago$markForVisibilityUpdate(Advancement advancement);
+    void archipelago$ensureVisibility(Advancement advancement);
 }
