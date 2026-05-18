25695df9c1f37af4764b57c5a9dd503bfb172555
Stuff691734 <23fuelln@gmail.com>
2026-05-18 16:21:36 -0500

    changes for 1.15.2 with ftb logic. see 1.12.2 for more details.

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -1,5 +1,7 @@
 package net.stuff691734.archipelago;
 
+import net.minecraft.ResourceLocationException;
+import io.github.archipelagomw.ClientStatus;
 import com.mojang.serialization.DataResult;
 import net.minecraft.advancements.AdvancementNode;
 import net.minecraft.advancements.AdvancementTree;
@@ -7,14 +9,21 @@ import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.stuff691734.archipelago.archipelagoData.CheckType;
 
+import javax.annotation.Nullable;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Utils {
     public static boolean isAdvancementId(String advancementId) {
+        if (Archipelago.getServer() == null) {
+            return false;
+        }
         DataResult<ResourceLocation> id = ResourceLocation.read(advancementId);
         if (id.isSuccess()) {
             AdvancementTree advancementManager = Archipelago.server.getAdvancements().tree();
@@ -39,18 +48,6 @@ public class Utils {
         return false;
     }
 
-    public static void giveItem(ServerPlayer player, String itemId) {
-        String[] strings = itemId.split(" ", 2);
-        int amount = Integer.parseInt(strings[0]);
-        String item = strings[1];
-        Item itemValue = BuiltInRegistries.ITEM.get(ResourceLocation.parse(item));
-        giveItem(player, itemValue, amount);
-    }
-
-    public static void giveItem(ServerPlayer player, Item item) {
-        giveItem(player, item, 1);
-    }
-
     public static void giveItem(ServerPlayer player, Item item, int amount) {
         ItemStack itemStack = new ItemStack(item, amount);
         if (!player.addItem(itemStack)) {
@@ -58,64 +55,111 @@ public class Utils {
         }
     }
 
-    public static void sendMessage(Component message) {
-        Archipelago.server.sendSystemMessage(message);
-
-        for(ServerPlayer player : Archipelago.server.getPlayerList().getPlayers()) {
-            player.sendSystemMessage(message);
+    public static void giveItem(MinecraftServer server, String item, @Nullable Long index) {
+        String[] strings = item.split(" ", 2);
+        int amount = Integer.parseInt(strings[0]);
+        String itemName = strings[1];
+        Item itemValue = ForgeRegistries.ITEMS.getValue(ResourceLocation.parse(itemName));
+        if (itemValue != null) {
+            giveItem(server, itemValue, amount, index);
         }
     }
 
-    public static boolean shouldAdvancementBeHidden(DisplayInfo display, AdvancementNode advancement) {
-        if (
-            Archipelago.slotData.isInitiated &&
-            (
-                !Archipelago.slotData.activated_modules.contains("Advancements") ||
-                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
-            )
-        ) {
-            if (
-                Archipelago.slotData.activated_modules.contains("Advancements") &&
-                !Archipelago.slotData.advancement_difficulty.contains(display.getType().getSerializedName())
-            ) {
-                return true;
-            }
+    public static void giveItem(MinecraftServer server, Item item, @Nullable Long index) {
+        giveItem(server, item, 1, index);
+    }
 
-            return display.isHidden();
-        }
-
-        if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
-            AdvancementNode rootAdvancement = AdvancementNode.getRoot(advancement);
-            String rootAdvancementName = rootAdvancement.holder().id().toString();
-
-            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(rootAdvancementName, false);
-        }
-        // parent advancement
-        else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
-            if (AdvancementNode.getRoot(advancement) == advancement) {
-                // if root check against self
-                return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
-            } else {
-                // otherwise check against values up tree not including self
-                AdvancementNode checkAdvancement = advancement;
-                // exits when all advancements up the tree have been checked
-                while (checkAdvancement != null) {
-                    checkAdvancement = checkAdvancement.parent();
-
-                    if (checkAdvancement != null) {
-                        String checkAdvancementName = checkAdvancement.holder().id().toString();
-                        if (!Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(checkAdvancementName, false)) {
-                            return true;
-                        }
+    public static void giveItem(MinecraftServer server, Item item, int amount, @Nullable Long index) {
+        for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+            if (index != null) {
+                if (ArchipelagoPersistentState.getInstance() != null) {
+                    if (ArchipelagoPersistentState.getInstance().playerLastCheck.getOrDefault(player.getCachedUniqueIdString(), 0) < index) {
+                        giveItem(player, item, amount);
                     }
                 }
-                return false;
+            } else {
+                giveItem(player, item, amount);
             }
         }
-        // not either tab or tree... invalid/notstarted, going to check against self as I eventually want
-        // to do an advancement insanity thing
-        else {
-            return !Archipelago.archipelagoPersistentState.advancementChecks.getOrDefault(advancement.holder().id().toString(), false);
+    }
+
+    public static Advancement getRoot(Advancement advancement) {
+        Advancement advancement1 = advancement;
+        while (true) {
+            Advancement advancement2 = advancement1.getParent();
+            if (advancement2 == null) {
+                return advancement1;
+            }
+            advancement1 = advancement2;
+        }
+    }
+
+    public static void sendMessage(Component message) {
+        Archipelago.executeOnServer((server) -> {
+            server.sendSystemMessage(message);
+
+            for(ServerPlayer player : server.getPlayerList().getPlayers()) {
+                player.sendSystemMessage(message);
+            }
+        });
+    }
+
+    public static boolean shouldAdvancementBeHidden(DisplayInfo display, Advancement advancement) {
+        if (display != null) {
+            if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
+                Advancement rootAdvancement = Utils.getRoot(advancement);
+                String rootAdvancementName = rootAdvancement.getId().toString();
+
+                return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(rootAdvancementName));
+            }
+            else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
+                if (Utils.getRoot(advancement) == advancement) {
+                    if (Archipelago.slotData.roots_unlocked) {
+                        return false;
+                    }
+                    return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.getId().toString()));
+                } else {
+                    Advancement checkAdvancement = advancement.getParent();
+                    while (checkAdvancement != null) {
+                        String checkAdvancementName = checkAdvancement.getId().toString();
+                        if (!ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(checkAdvancementName))) {
+                            return true;
+                        }
+                        checkAdvancement = checkAdvancement.getParent();
+                    }
+                    return false;
+                }
+            }
+            // not either, probably uninitiated
+            else {
+                return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.getId().toString()));
+            }
+        }
+        return false;
+    }
+
+    public static Long getLocationId(String locationName) {
+        return Archipelago.client.getDataPackage().getGame("Modded Minecraft")
+                .locationNameToId.keySet()
+                .stream().filter(
+                    (key) -> locationName.equals(String.format("%s %s", (Object[]) key.split(" ")))
+                ).findFirst().map(
+                    (value) -> Archipelago.client.getDataPackage().getGame("Modded Minecraft").locationNameToId.get(value)
+                ).orElse(null);
+    }
+
+    public static void sendCheck(String checkName) {
+        if (Archipelago.client.isConnected()) {
+            Long check_id = Utils.getLocationId(checkName);
+            if (check_id != null) {
+                Archipelago.client.getLocationManager().checkLocation(check_id);
+                if (Archipelago.slotData.isCheckFinalGoal(checkName)) {
+                    Archipelago.client.setGameState(ClientStatus.CLIENT_GOAL);
+                }
+            }
+        } else if (ArchipelagoPersistentState.getInstance() != null) {
+            ArchipelagoPersistentState.getInstance().pendingChecks.add(checkName);
+            ArchipelagoPersistentState.getInstance().setDirty(true);
         }
     }
 }
--- src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/client/gui/quests/QuestButtonMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/client/gui/quests/QuestButtonMixin.java
@@ -1,13 +1,11 @@
 package net.stuff691734.archipelago.mixin.FTBQuests.client.gui.quests;
 
 import dev.ftb.mods.ftblibrary.icon.Icon;
-import dev.ftb.mods.ftbquests.api.FTBQuestsAPI;
-import dev.ftb.mods.ftbquests.client.ClientQuestFile;
 import dev.ftb.mods.ftbquests.client.gui.quests.QuestButton;
 import dev.ftb.mods.ftbquests.quest.Quest;
-import dev.ftb.mods.ftbquests.quest.theme.property.ThemeProperties;
+import dev.ftb.mods.ftbquests.quest.TeamData;
 import net.minecraft.client.Minecraft;
-import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.mixinHelper.FTBQuestsMixinHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,20 +21,22 @@ public class QuestButtonMixin {
     // function after quest icon is set but before getting drawn
     @ModifyVariable(method = "draw", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftblibrary/ui/GuiHelper;setupDrawing()V"), remap = false, name = "questIcon")
     public Icon drawAlertIcon(Icon questIcon) {
-        if (Archipelago.slotData.isFTBQuestRewardRandomized(this.quest.getShape())) {
-            // only modify if it is a quest and it is randomized and we randomized rewards
-            if (
-                Archipelago.archipelagoPersistentState.ftbQuestChecks.getOrDefault(this.quest.getCodeString(), false) &&
-                // sadly this undoes some of the optimization done by the ftb team, but teamData.hasUnclaimedRewards returns false when not having access
-                this.quest.getRewards().stream().anyMatch(
-                    reward -> !((ClientQuestFile) FTBQuestsAPI.api().getQuestFile(true)).selfTeamData.isRewardClaimed(Minecraft.getInstance().player.getUUID(), reward)
-                )
-            ) {
-                // got this check but haven't claimed yet
-                return ThemeProperties.ALERT_ICON.get(this.quest);
-            }
-        }
-        return questIcon;
+        assert Minecraft.getInstance().player != null;
+        return FTBQuestsMixinHelper.getQuestIcon(this.quest, questIcon, TeamData.get(Minecraft.getInstance().player));
+//        if (Archipelago.slotData.isFTBQuestRewardRandomized(this.quest.getShape())) {
+//            // only modify if it is a quest and it is randomized and we randomized rewards
+//            assert Minecraft.getInstance().player != null; // would've already errored in QuestButton.draw
+//            if (
+//                Archipelago.archipelagoPersistentState.ftbQuestChecks.getOrDefault(this.quest.getCodeString(), false) &&
+//                // sadly this undoes some of the optimization done by the ftb team, but teamData.hasUnclaimedRewards returns false when not having access
+//                this.quest.rewards.stream().anyMatch(
+//                    reward -> !TeamData.get(Minecraft.getInstance().player).isRewardClaimed(Minecraft.getInstance().player.getUUID(), reward)
+//                )
+//            ) {
+//                // got this check but haven't claimed yet
+//                return ThemeProperties.ALERT_ICON.get(this.quest);
+//            }
+//        }
+//        return questIcon;
     }
-
 }
