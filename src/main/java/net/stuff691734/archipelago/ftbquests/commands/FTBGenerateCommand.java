aedfd519f74ff2f999a905a5b1eace1d3a895bae
Stuff691734 <23fuelln@gmail.com>
2026-05-20 11:53:08 -0500

    fix how generated output looks (string value instead of Component thing) for 1.16.4

--- src/main/java/net/stuff691734/archipelago/ftbquests/commands/FTBGenerateCommand.java
+++ src/main/java/net/stuff691734/archipelago/ftbquests/commands/FTBGenerateCommand.java
@@ -4,12 +4,14 @@ import com.feed_the_beast.ftbquests.quest.Chapter;
 import com.feed_the_beast.ftbquests.quest.Quest;
 import com.feed_the_beast.ftbquests.quest.QuestObject;
 import com.feed_the_beast.ftbquests.quest.ServerQuestFile;
-import com.feed_the_beast.ftbquests.quest.task.AdvancementTask;
-import com.feed_the_beast.ftbquests.quest.task.Task;
-import com.feed_the_beast.ftbquests.quest.task.FTBQuestsTasks;
+import com.feed_the_beast.ftbquests.quest.task.*;
+import com.feed_the_beast.mods.ftbguilibrary.utils.StringUtils;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.stuff691734.archipelago.Utils;
 import net.stuff691734.archipelago.archipelagoData.DependencyNotation;
 import net.stuff691734.archipelago.archipelagoData.FTBQuestsCheck;
@@ -33,7 +35,7 @@ public class FTBGenerateCommand {
                 } else {
                     questDependencies.setMinimum(0);
                 }
-                getDependencies(questDependencies, quest);
+                getDependencies(questDependencies, quest, server);
                 dependencies.addNested(questDependencies);
 
                 for (Task task : quest.tasks) {
@@ -50,11 +52,11 @@ public class FTBGenerateCommand {
                 }
 
                 ftbQuestsChecks.put(
-                        String.format("ftb %s (%s)", quest.getCodeString(), quest.getTitle()),
+                        String.format("ftb %s (%s)", quest.getCodeString(), getName(quest, server)),
                         new FTBQuestsCheck(
                                 quest.getShape(),
                                 dependencies,
-                                String.format("ftb %s (%s)", quest.getChapter().getCodeString(), quest.getChapter().getTitle())
+                                String.format("ftb %s (%s)", quest.getChapter().getCodeString(), getName(quest.getChapter(), server))
                         )
                 );
             }
@@ -72,20 +74,97 @@ public class FTBGenerateCommand {
             );
     }
 
-    private static void getDependencies(DependencyNotation input, Quest quest) {
+    private static void getDependencies(DependencyNotation input, Quest quest, MinecraftServer server) {
         for (QuestObject questObject : quest.dependencies) {
             if (questObject instanceof Chapter) {
                 DependencyNotation chapterDependency = new DependencyNotation();
                 for (Quest chapterQuest : ((Chapter) questObject).quests) {
-                    chapterDependency.addCheck(String.format("ftb %s (%s)", chapterQuest.getCodeString(), chapterQuest.getTitle()));
+                    chapterDependency.addCheck(String.format("ftb %s (%s)", chapterQuest.getCodeString(), getName(chapterQuest, server)));
                 }
                 input.addNested(chapterDependency);
             } else {
                 if (questObject instanceof Task) {
                     questObject = ((Task) questObject).quest;
                 }
-                input.addCheck(String.format("ftb %s (%s)", questObject.getCodeString(), questObject.getTitle()));
+                input.addCheck(String.format("ftb %s (%s)", questObject.getCodeString(), getName(questObject, server)));
             }
         }
+    }
+
+    private static String getName(QuestObject questObject, MinecraftServer server) {
+        // getAltTitle (used for default quest names) is client side only, so I have reimplemented them here *sigh*
+        // This is mostly copy and pasted code from
+        if (!questObject.title.isEmpty()) {
+            return questObject.title;
+        }
+        if (questObject instanceof Quest && !((Quest) questObject).tasks.isEmpty()) {
+            Task task = ((Quest)questObject).tasks.get(0);
+            if (task.getType() == FTBQuestsTasks.ADVANCEMENT) {
+                AdvancementTask task1 = (AdvancementTask) task;
+                if (Utils.isAdvancementId(task1.advancement)) {
+                    Advancement advancement = server.getAdvancements().getAdvancement(new ResourceLocation(task1.advancement));
+                    if (advancement != null && advancement.getDisplay() != null) {
+                        ITextComponent text = (new TranslationTextComponent("ftbquests.task.ftbquests.advancement")).append(": ").append(advancement.getDisplay().getTitle());
+                        return text.getString();
+                    }
+                }
+                return task.getType().getDisplayName().getString();
+            }
+            if (task.getType() == FTBQuestsTasks.BIOME) {
+                BiomeTask task1 = (BiomeTask) task;
+                ITextComponent text = (new TranslationTextComponent("ftbquests.task.ftbquests.biome")).append(": ").append(task1.biome.location().toString());
+                return text.getString();
+
+            }
+            if (task.getType() == FTBQuestsTasks.CHECKMARK) {
+                // does not override
+            }
+            if (task.getType() == FTBQuestsTasks.CUSTOM) {
+                // does not override
+            }
+            if (task.getType() == FTBQuestsTasks.DIMENSION) {
+                DimensionTask task1 = (DimensionTask) task;
+                ITextComponent text = (new TranslationTextComponent("ftbquests.task.ftbquests.dimension")).append(": ").append(task1.dimension.location().toString());
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.ITEM) {
+                ItemTask task1 = (ItemTask) task;
+                ITextComponent text = task1.count > 1L ? (new StringTextComponent(task1.count + "x ")).append(task1.item.getHoverName()) : (new StringTextComponent("")).append(task1.item.getHoverName());
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.KILL) {
+                KillTask task1 = (KillTask) task;
+                ITextComponent text = new TranslationTextComponent("ftbquests.task.ftbquests.kill.title", new Object[]{task1.getMaxProgressString(), new TranslationTextComponent("entity." + task1.entity.getNamespace() + "." + task1.entity.getPath())});
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.LOCATION) {
+                // does not override
+            }
+            if (task.getType() == FTBQuestsTasks.OBSERVATION) {
+                // does not override
+            }
+            if (task.getType() == FTBQuestsTasks.STAT) {
+                StatTask task1 = (StatTask) task;
+                ITextComponent text = new TranslationTextComponent("stat." + task1.stat.getNamespace() + "." + task1.stat.getPath());
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.XP) {
+                XPTask task1 = (XPTask) task;
+                ITextComponent text = (new TranslationTextComponent("ftbquests.reward.ftbquests.xp_levels")).append(": ").append(task1.getMaxProgressString());
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.FLUID) {
+                FluidTask task1 = (FluidTask) task;
+                ITextComponent text = (new StringTextComponent(FluidTask.getVolumeString(task1.amount) + " of ")).append(task1.createFluidStack().getDisplayName());
+                return text.getString();
+            }
+            if (task.getType() == FTBQuestsTasks.FORGE_ENERGY) {
+                ForgeEnergyTask task1 = (ForgeEnergyTask) task;
+                ITextComponent text = new TranslationTextComponent("ftbquests.task.ftbquests.forge_energy.text", new Object[]{StringUtils.formatDouble((double)task1.value, true)});
+                return text.getString();
+            }
+            return task.getType().getDisplayName().getString();
+        }
+        return new TranslationTextComponent("ftbquests.unnamed").getString();
     }
 }
