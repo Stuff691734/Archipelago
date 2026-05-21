10f88a3bfe25383560f1ca6e00d28db30464c536
Stuff691734 <23fuelln@gmail.com>
2026-05-21 09:43:38 -0500

    changes for 1.17.1 without ftb logic. see 1.12.2 for more details.

---
+++ src/main/java/net/stuff691734/archipelago/mixinHelper/MixinHelper.java
@@ -0,0 +1,76 @@
+package net.stuff691734.archipelago.mixinHelper;
+
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.client.gui.screens.advancements.AdvancementTab;
+import net.minecraft.client.gui.screens.advancements.AdvancementWidget;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ArchipelagoPersistentState;
+import net.stuff691734.archipelago.Utils;
+import net.stuff691734.archipelago.archipelagoData.CheckType;
+
+import java.util.Objects;
+
+// methods used by Mixin to make code more consistent between coremods and mixins
+public class MixinHelper {
+    public static boolean shouldBeVisible(Advancement advancement) {
+        if (advancement.getDisplay() != null) {
+            if (Archipelago.slotData.isInitiated &&
+                (
+                    !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                    !Archipelago.slotData.advancement_difficulty.contains(advancement.getDisplay().getFrame().getName())
+                )
+            ) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static void sendArchipelagoAdvancement(Advancement advancement) {
+        Utils.sendCheck(CheckType.ADVANCEMENT.addPrefix(advancement.getId().toString()));
+    }
+
+    public static boolean allowAdvancementCompletion(Advancement advancement) {
+        return !Utils.shouldAdvancementBeHidden(advancement.getDisplay(), advancement);
+    }
+
+    public static AdvancementWidget getGuiAdvancementParent(AdvancementWidget parent, DisplayInfo displayInfo, Advancement advancement) {
+        if (parent == null) return null;
+        if (Archipelago.slotData.isInitiated) {
+            if (
+                Archipelago.slotData.activated_modules.contains("Advancements") &&
+                !Archipelago.slotData.advancement_difficulty.contains(displayInfo.getFrame().getName())
+            ) {
+                return null;
+            }
+            else if (!Archipelago.slotData.activated_modules.contains("Advancements")) {
+                return displayInfo.isHidden() ? null : parent;
+            }
+        }
+        if (Utils.shouldAdvancementBeHidden(displayInfo, advancement)) {
+            return null;
+        }
+        return parent;
+    }
+
+    public static AdvancementTab getGuiAdvancementTab(AdvancementTab tab) {
+        if (tab == null) {
+            return null;
+        }
+        if (Archipelago.slotData.isInitiated &&
+            (
+                !Archipelago.slotData.activated_modules.contains("Advancements") ||
+                (Objects.equals(Archipelago.slotData.unlock_type, "tree") && Archipelago.slotData.roots_unlocked)
+            )
+        ) {
+            return tab;
+        }
+        if (!ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(tab.getAdvancement().getId().toString()))) {
+            return null;
+        }
+
+        return tab;
+    }
+}
