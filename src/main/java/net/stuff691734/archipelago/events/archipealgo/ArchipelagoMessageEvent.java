363fe4a43ff8f084469a8e88f54fed9a1d8234f1
Stuff691734 <23fuelln@gmail.com>
2026-05-21 09:43:38 -0500

    changes for 1.17.1 without ftb logic. see 1.12.2 for more details.

--- src/main/java/net/stuff691734/archipelago/events/archipealgo/ArchipelagoMessageEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/archipealgo/ArchipelagoMessageEvent.java
@@ -3,6 +3,7 @@ package net.stuff691734.archipelago.events.archipealgo;
 import io.github.archipelagomw.events.ArchipelagoEventListener;
 import io.github.archipelagomw.events.PrintJSONEvent;
 import net.minecraft.network.chat.TextComponent;
+import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.Utils;
 
 public class ArchipelagoMessageEvent {
