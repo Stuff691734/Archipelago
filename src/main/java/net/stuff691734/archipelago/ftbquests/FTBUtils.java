ebb184473628883b09098a0245ab01b0eae11477
Stuff691734 <23fuelln@gmail.com>
2026-05-21 14:55:01 -0500

    fix checking if user has checks required for chapter group

--- src/main/java/net/stuff691734/archipelago/ftbquests/FTBUtils.java
+++ src/main/java/net/stuff691734/archipelago/ftbquests/FTBUtils.java
@@ -1,9 +1,6 @@
 package net.stuff691734.archipelago.ftbquests;
 
-import dev.ftb.mods.ftbquests.quest.Chapter;
-import dev.ftb.mods.ftbquests.quest.Quest;
-import dev.ftb.mods.ftbquests.quest.QuestObject;
-import dev.ftb.mods.ftbquests.quest.ServerQuestFile;
+import dev.ftb.mods.ftbquests.quest.*;
 import dev.ftb.mods.ftbquests.quest.task.Task;
 import net.stuff691734.archipelago.Archipelago;
 import net.stuff691734.archipelago.ArchipelagoPersistentState;
@@ -46,6 +43,10 @@ public class FTBUtils {
         if (questObject instanceof Task) {
             // operate on quest that the task belongs to
             return ArchipelagoPersistentState.getCheck(CheckType.FTB_QUEST.addPrefix(((Task) questObject).quest.getCodeString()));
+        }
+
+        if (questObject instanceof ChapterGroup) {
+            return ((ChapterGroup) questObject).chapters.stream().allMatch(FTBUtils::hasRequiredChecks);
         }
 
         if (questObject instanceof Chapter) {
