7e41ba7d0657d922b89c3083fcd005a4c43d1c8c
Stuff691734 <23fuelln@gmail.com>
2026-05-21 09:43:38 -0500

    changes for 1.17.1 without ftb logic. see 1.12.2 for more details.

---
+++ src/main/java/net/stuff691734/archipelago/net/GetCheckPacket.java
@@ -0,0 +1,38 @@
+package net.stuff691734.archipelago.net;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fmllegacy.network.NetworkEvent;
+import net.stuff691734.archipelago.Archipelago;
+
+import java.util.function.Supplier;
+
+public class GetCheckPacket {
+    public String check;
+
+    public GetCheckPacket(String check) {
+        this.check = check;
+    }
+
+    public GetCheckPacket(FriendlyByteBuf friendlyByteBuf) {
+        this.check = friendlyByteBuf.readString();
+    }
+
+    public void encode(FriendlyByteBuf friendlyByteBuf) {
+        friendlyByteBuf.writeString(check);
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> {
+            DistExecutor.runWhenOn(
+                    Dist.CLIENT,
+                    () -> () -> {
+                        Archipelago.LOGGER.info("Received archipelago check from server.");
+                        Archipelago.clientState.addCheck(this.check);
+                    }
+            );
+        });
+        context.get().setPacketHandled(true);
+    }
+}
---
+++ src/main/java/net/stuff691734/archipelago/net/StartSyncChecksPacket.java
@@ -0,0 +1,46 @@
+package net.stuff691734.archipelago.net;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fmllegacy.network.NetworkEvent;
+import net.stuff691734.archipelago.Archipelago;
+
+import java.util.function.Supplier;
+
+public class StartSyncChecksPacket {
+    private String[] checks;
+
+    public StartSyncChecksPacket(String[] checks) {
+        this.checks = checks;
+    }
+
+    public StartSyncChecksPacket(FriendlyByteBuf friendlyByteBuf) {
+        int checksLength = friendlyByteBuf.readInt();
+        checks = new String[checksLength];
+        for (int i = 0; i < checksLength; i++) {
+            checks[i] = friendlyByteBuf.readString();
+        }
+    }
+
+    public void encode(FriendlyByteBuf friendlyByteBuf) {
+        friendlyByteBuf.writeInt(checks.length);
+        for (String check : checks) {
+            friendlyByteBuf.writeString(check);
+        }
+    }
+
+    public void handle(Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> {
+            DistExecutor.runWhenOn(
+                    Dist.CLIENT,
+                    () -> () -> {
+                        Archipelago.LOGGER.info("Got archipelago check data from server.");
+
+                        Archipelago.clientState.addAllChecks(this.checks);
+                    }
+            );
+        });
+        context.get().setPacketHandled(true);
+    }
+}
