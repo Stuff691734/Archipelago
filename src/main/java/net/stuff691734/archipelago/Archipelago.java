6bb5ac673c4e6f975e9571ec17ae4935cb1d4605
Stuff691734 <23fuelln@gmail.com>
2026-05-21 10:20:36 -0500

    fixes for 1.17.1

--- src/main/java/net/stuff691734/archipelago/Archipelago.java
+++ src/main/java/net/stuff691734/archipelago/Archipelago.java
@@ -5,6 +5,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.stuff691734.archipelago.events.mod.ModEvents;
 import net.stuff691734.archipelago.events.neoforge.ForgeEvents;
 import org.slf4j.Logger;
 
@@ -25,6 +27,7 @@ public class Archipelago {
 
     public Archipelago() {
         ForgeEvents.register(MinecraftForge.EVENT_BUS);
+        ModEvents.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
 
