daf5be786a1d26bf510ac9d2a6267d2e7cadf41d
Stuff691734 <23fuelln@gmail.com>
2026-05-26 10:53:55 -0500

    changes for 1.20.2 (switch to neoforge, changing network packets)

--- src/main/java/net/stuff691734/archipelago/Utils.java
+++ src/main/java/net/stuff691734/archipelago/Utils.java
@@ -3,7 +3,6 @@ package net.stuff691734.archipelago;
 import com.mojang.serialization.DataResult;
 import io.github.archipelagomw.ClientStatus;
 import net.minecraft.advancements.AdvancementNode;
-import net.minecraft.advancements.AdvancementTree;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
@@ -12,7 +11,6 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.stuff691734.archipelago.archipelagoData.CheckType;
 
 import javax.annotation.Nullable;
@@ -58,10 +56,8 @@ public class Utils {
         String[] strings = item.split(" ", 2);
         int amount = Integer.parseInt(strings[0]);
         String itemName = strings[1];
-        Item itemValue = ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName));
-        if (itemValue != null) {
-            giveItem(server, itemValue, amount, index);
-        }
+        Item itemValue = BuiltInRegistries.ITEM.get(new ResourceLocation(itemName));
+        giveItem(server, itemValue, amount, index);
     }
 
     public static void giveItem(MinecraftServer server, Item item, @Nullable Long index) {
@@ -82,10 +78,10 @@ public class Utils {
         }
     }
 
-    public static Advancement getRoot(Advancement advancement) {
-        Advancement advancement1 = advancement;
+    public static AdvancementNode getRoot(AdvancementNode advancement) {
+        AdvancementNode advancement1 = advancement;
         while (true) {
-            Advancement advancement2 = advancement1.getParent();
+            AdvancementNode advancement2 = advancement1.parent();
             if (advancement2 == null) {
                 return advancement1;
             }
@@ -103,35 +99,35 @@ public class Utils {
         });
     }
 
-    public static boolean shouldAdvancementBeHidden(DisplayInfo display, Advancement advancement) {
+    public static boolean shouldAdvancementBeHidden(DisplayInfo display, AdvancementNode advancement) {
         if (display != null) {
             if (Objects.equals(Archipelago.slotData.unlock_type, "tab")) {
-                Advancement rootAdvancement = Advancement.getRoot(advancement);
-                String rootAdvancementName = rootAdvancement.getId().toString();
+                AdvancementNode rootAdvancement = Utils.getRoot(advancement);
+                String rootAdvancementName = rootAdvancement.holder().id().toString();
 
                 return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(rootAdvancementName));
             }
             else if (Objects.equals(Archipelago.slotData.unlock_type, "tree")) {
-                if (Advancement.getRoot(advancement) == advancement) {
+                if (Utils.getRoot(advancement) == advancement) {
                     if (Archipelago.slotData.roots_unlocked) {
                         return false;
                     }
-                    return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.getId().toString()));
+                    return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.holder().id().toString()));
                 } else {
-                    Advancement checkAdvancement = advancement.getParent();
+                    AdvancementNode checkAdvancement = advancement.parent();
                     while (checkAdvancement != null) {
-                        String checkAdvancementName = checkAdvancement.getId().toString();
+                        String checkAdvancementName = checkAdvancement.holder().id().toString();
                         if (!ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(checkAdvancementName))) {
                             return true;
                         }
-                        checkAdvancement = checkAdvancement.getParent();
+                        checkAdvancement = checkAdvancement.parent();
                     }
                     return false;
                 }
             }
             // not either, probably uninitiated
             else {
-                return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.getId().toString()));
+                return !ArchipelagoPersistentState.getCheck(CheckType.ADVANCEMENT.addPrefix(advancement.holder().id().toString()));
             }
         }
         return false;
