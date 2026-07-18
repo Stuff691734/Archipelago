092458aac42093b4684a5b6b1aa1a6c33ca04eec
Stuff691734 <23fuelln@gmail.com>
2026-07-17 20:37:43 -0500

    Major refactoring to separate out version independent code. see https://github.com/Stuff691734/archipelago-lib for code that has been separated.

--- src/main/java/net/stuff691734/archipelago/mixin/AdvancementWidgetMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/AdvancementWidgetMixin.java
@@ -3,8 +3,8 @@ package net.stuff691734.archipelago.mixin;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.client.gui.screens.advancements.AdvancementWidget;
-import net.stuff691734.archipelago.Utils;
-import net.stuff691734.archipelago.mixinHelper.MixinHelper;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.implementations.AdvancementImpl;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,26 +19,23 @@ public class AdvancementWidgetMixin {
     @Shadow
     @Final
     private Advancement advancement;
+
     @Shadow
     @Nullable
     private AdvancementWidget parent;
 
-    @Shadow
-    @Final
-    private DisplayInfo display;
-
     @Redirect(method = "draw", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
     public boolean drawSetNotHidden(DisplayInfo display) {
-        return Utils.shouldAdvancementBeHidden(display, this.advancement);
+        return !Archipelago.logic.shouldShowAdvancement(new AdvancementImpl(this.advancement));
     }
 
     @Redirect(method = "isMouseOver", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/DisplayInfo;isHidden()Z"))
     public boolean isMouseOverSetNotHidden(DisplayInfo display) {
-        return Utils.shouldAdvancementBeHidden(display, this.advancement);
+        return !Archipelago.logic.shouldShowAdvancement(new AdvancementImpl(this.advancement));
     }
 
     @Redirect(method = "drawConnectivity", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screens/advancements/AdvancementWidget;parent:Lnet/minecraft/client/gui/screens/advancements/AdvancementWidget;", opcode = Opcodes.GETFIELD))
     public AdvancementWidget parent(AdvancementWidget thisWidget) {
-        return MixinHelper.getGuiAdvancementParent(this.parent, this.display, this.advancement);
+        return Archipelago.logic.isDependencyDrawn(this.parent, new AdvancementImpl(this.advancement));
     }
 }
