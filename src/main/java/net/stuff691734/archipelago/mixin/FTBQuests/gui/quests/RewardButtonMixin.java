280ae28a6cf5e329640e55f26bffc374cdc5d182
Stuff691734 <23fuelln@gmail.com>
2026-07-17 20:37:43 -0500

    Major refactoring to separate out version independent code. see https://github.com/Stuff691734/archipelago-lib for code that has been separated.

--- src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/gui/quests/RewardButtonMixin.java
+++ src/main/java/net/stuff691734/archipelago/mixin/FTBQuests/gui/quests/RewardButtonMixin.java
@@ -4,7 +4,8 @@ import dev.ftb.mods.ftbquests.gui.quests.RewardButton;
 import dev.ftb.mods.ftbquests.quest.Quest;
 import dev.ftb.mods.ftbquests.quest.QuestObject;
 import dev.ftb.mods.ftbquests.quest.TeamData;
-import net.stuff691734.archipelago.mixinHelper.FTBQuestsMixinHelper;
+import net.stuff691734.archipelago.Archipelago;
+import net.stuff691734.archipelago.ftbquests.implementations.FTBQuestsImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,12 +14,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class RewardButtonMixin {
     @Redirect(method = "draw", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbquests/quest/TeamData;isCompleted(Ldev/ftb/mods/ftbquests/quest/QuestObject;)Z"), remap = false)
     private boolean modifyRewardAccess(TeamData teamData, QuestObject questObject) {
-        return FTBQuestsMixinHelper.isQuestRewardAvailable((Quest) questObject, teamData);
+        return Archipelago.logic.isFTBQuestRewardObtained(new FTBQuestsImpl((Quest) questObject), teamData.isCompleted(questObject));
     }
 
     @Redirect(method = "getWidgetType", at = @At(value = "INVOKE", target = "Ldev/ftb/mods/ftbquests/quest/TeamData;isCompleted(Ldev/ftb/mods/ftbquests/quest/QuestObject;)Z"), remap = false)
     public boolean getWidgetType(TeamData teamData, QuestObject questObject) {
         // required for allowing user to click on quest reward and get reward
-        return FTBQuestsMixinHelper.isQuestRewardAvailable((Quest) questObject, teamData);
+        return Archipelago.logic.isFTBQuestRewardObtained(new FTBQuestsImpl((Quest) questObject), teamData.isCompleted(questObject));
     }
 }
