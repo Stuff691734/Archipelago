f2b483d9bf5692fc6e74c28bcad899b3e617ebbe
Stuff691734 <23fuelln@gmail.com>
2026-07-19 11:56:53 -0500

    bump dependency archipelago-lib version to 0.11.4

--- src/main/java/net/stuff691734/archipelago/ftbquests/implementations/FTBQuestsImpl.java
+++ src/main/java/net/stuff691734/archipelago/ftbquests/implementations/FTBQuestsImpl.java
@@ -105,6 +105,16 @@ public class FTBQuestsImpl implements FTBQuestsInterface {
         return this.getTitle(server);
     }
 
+    @Override
+    public boolean hasRewards() {
+        return !this.quest.getRewards().isEmpty();
+    }
+
+    @Override
+    public boolean isInvisibleUntilCompleted() {
+        return ((QuestAccessor) (Object) this.quest).archipelago$isInvisibleUntilCompleted();
+    }
+
     private String getTitle(ServerInterface server) {
         // getAltTitle (used for default quest names) is client side only, so I have reimplemented them here *sigh*
         // This is mostly copy and pasted code from
