7d5f4e70287d216435738579d133f43fb346a746
Stuff691734 <23fuelln@gmail.com>
2026-08-08 18:49:17 -0500

    Major refactoring to separate out version independent code. see https://github.com/Stuff691734/archipelago-lib for code that has been separated.

--- src/main/java/net/stuff691734/archipelago/events/neoforge/ServerStoppingEvent.java
+++ src/main/java/net/stuff691734/archipelago/events/neoforge/ServerStoppingEvent.java
@@ -3,12 +3,16 @@ package net.stuff691734.archipelago.events.neoforge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
 import net.stuff691734.archipelago.Archipelago;
-import net.stuff691734.archipelago.SlotData;
+import net.stuff691734.archipelago.ArchipelagoPersistentState;
+import net.stuff691734.archipelagoLib.SlotData;
 
 public class ServerStoppingEvent {
     @SubscribeEvent
     public void onEvent(FMLServerStoppingEvent event) {
         Archipelago.client.close();
         Archipelago.slotData = new SlotData();
+        Archipelago.CLIENT_STATE.clear();
+        Archipelago.setServer(null);
+        ArchipelagoPersistentState.clearInstance();
     }
 }
