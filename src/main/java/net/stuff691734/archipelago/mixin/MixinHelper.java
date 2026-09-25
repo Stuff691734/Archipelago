fcba4fa70eb6d785052daae932e4d4ac77c76fa2
Stuff691734 <23fuelln@gmail.com>
2026-09-24 20:22:18 -0500

    fix advancement rendering

--- src/main/java/net/stuff691734/archipelago/mixin/MixinHelper.java
+++ src/main/java/net/stuff691734/archipelago/mixin/MixinHelper.java
@@ -8,9 +8,6 @@ import net.stuff691734.archipelago.implementations.AdvancementImpl;
 // methods used by coreMod to help write less bytecode
 public class MixinHelper {
     public static boolean isHidden(DisplayInfo displayInfo, Advancement advancement) {
-        if (Archipelago.logic.shouldShowAdvancement(new AdvancementImpl(advancement))) {
-            return false;
-        }
-        return displayInfo.isHidden();
+        return !Archipelago.logic.shouldShowAdvancement(new AdvancementImpl(advancement));
     }
 }
